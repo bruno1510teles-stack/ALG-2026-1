56077105b8edaa2e300ab01c80aa8ee56a20626f
Lucas Ruffo <lucas.ruffo@itlab.com.br>
2023-10-06 16:54:00 -0300

    teste

--- dags/dags_folder_1/example_other.py
+++ dags/dags_folder_1/example_other.py
@@ -27,7 +27,7 @@ from airflow.models.baseoperator import chain
 from airflow.operators.bash import BashOperator
 
 with models.DAG(
-    dag_id="example_complex",
+    dag_id="example_complex_folder_1",
     schedule=None,
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
