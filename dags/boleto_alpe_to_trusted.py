38c524673eddd321f940728c8805429afa328bd5
felipe.mayer <felipe.mayer@alpe.com.br>
2024-01-29 10:39:43 -0300

    Adicionado verificação se os boletos do dia tem o filtro

--- dags/boleto_alpe_to_trusted.py
+++ dags/boleto_alpe_to_trusted.py
@@ -19,8 +19,10 @@ BOLETOS_ALPE_RAW_FOLDER = f"topics/opdb.ccred_schema_{ Variable.get('STAGE') }_d
 now = datetime.now(tz=timezone(timedelta(hours=-3)))
 yesterday = now - timedelta(days=1)
 
-#day_to_process = f"{BOLETOS_ALPE_RAW_FOLDER}year={yesterday.year}/month={str(yesterday.month).zfill(2)}/day={str(yesterday.day).zfill(2)}/"
-day_to_process= f"{BOLETOS_ALPE_RAW_FOLDER}year=2023/month=11/day=21/"
+day_to_process = f"{BOLETOS_ALPE_RAW_FOLDER}year={yesterday.year}/month={str(yesterday.month).zfill(2)}/day={str(yesterday.day).zfill(2)}/"
+
+if { Variable.get('STAGE') } == 'dev':
+    day_to_process= f"{BOLETOS_ALPE_RAW_FOLDER}year=2023/month=11/day=21/"
 
 # DEFINE FUNCTIONS
 def check_files_to_processed(files_to_process):
