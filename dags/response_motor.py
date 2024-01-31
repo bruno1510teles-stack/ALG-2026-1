ce74f698c3736bf863ce483fb53428dd952ff785
felipe.mayer <felipe.mayer@alpe.com.br>
2024-01-31 14:06:02 -0300

    rodando a reposta do motor para todas as entradas de 2024/01/2x.

--- dags/response_motor.py
+++ dags/response_motor.py
@@ -19,7 +19,7 @@ RESPONSE_MOTOR_RAW_FOLDER = f"topics/opdb.inrp_{ Variable.get('STAGE') }_default
 now_tz = datetime.now(tz=timezone(timedelta(hours=-3)))
 yesterday = now_tz - timedelta(days=1)
 # day_to_process = f"{RESPONSE_MOTOR_RAW_FOLDER}year={yesterday.year}/month={str(yesterday.month).zfill(2)}/day={str(yesterday.day).zfill(2)}/"
-day_to_process = f"{RESPONSE_MOTOR_RAW_FOLDER}year=2024/month=01/day=1"
+day_to_process = f"{RESPONSE_MOTOR_RAW_FOLDER}year=2024/month=01/day=2"
 
 
 # DEFINE FUNCTIONS
