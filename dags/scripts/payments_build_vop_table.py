5f029b1abd74697d53746196e2fa05aadaa2c55a
João Leite <joao.leite@alpe.com.br>
2024-02-15 15:12:00 -0300

    Script de tratamento importado alterado

--- dags/scripts/payments_build_vop_table.py
+++ dags/scripts/payments_build_vop_table.py
@@ -5,7 +5,7 @@ from io import BytesIO
 import os
 from deltalake import write_deltalake, DeltaTable
 
-from scripts.payments_refined_engine_v2 import transform_payments_engine_v2
+from scripts.payments_build_vop_table import transform_data_to_refined
 
 now = datetime.now(tz=timezone(timedelta(hours=-3)))
 yesterday = now - timedelta(days=1)
