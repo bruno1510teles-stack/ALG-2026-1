8d801c98a4bb32231a4d7f1d36b9ca0de53f5a41
João Leite <joao.leite@alpe.com.br>
2024-02-29 16:29:24 -0300

    Alterando data para rodar

--- dags/visao_resumida_hp_externa.py
+++ dags/visao_resumida_hp_externa.py
@@ -36,7 +36,7 @@ default_args = {
 
 # DEFINE DAG
 @dag(
-    start_date=datetime(2024, 3, 1), # definir quando for rodar automatico
+    start_date=datetime(2024, 2, 1), # definir quando for rodar automatico
     max_active_runs=1,
     schedule_interval='0 10 * * *',
     default_args=default_args,
