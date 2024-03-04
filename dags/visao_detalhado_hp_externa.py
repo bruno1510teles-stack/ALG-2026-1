ba881d57acb8e3060eee3fa6b356556fcc16305d
felipe.ferraz <felipe.ferraz@alpe.com.br>
2024-03-04 14:35:23 -0300

    ajustando mês 02 para 2

--- dags/visao_detalhado_hp_externa.py
+++ dags/visao_detalhado_hp_externa.py
@@ -18,7 +18,7 @@ BOLETOS_ALPE_TRUSTED_FOLDER = "boletos/"
 
 now = datetime.now(tz=timezone(timedelta(hours=-3)))
 yesterday = '2024-02-28'
-day_to_process = f"{BOLETOS_ALPE_TRUSTED_FOLDER}year=2024/month=02/day=28/"
+day_to_process = f"{BOLETOS_ALPE_TRUSTED_FOLDER}year=2024/month=2/day=28/"
 
 
 # DEFINE FUNCTIONS
