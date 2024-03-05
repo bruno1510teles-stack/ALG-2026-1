9cd24e95744c42501b8e7e3cf04a7d2b9802464e
felipe.ferraz <felipe.ferraz@alpe.com.br>
2024-03-05 12:33:28 -0300

    deixando somente 1 variável para ver se processa

--- dags/visao_detalhada_hp_externa.py
+++ dags/visao_detalhada_hp_externa.py
@@ -7,7 +7,7 @@ from airflow.operators.empty import EmptyOperator
 from airflow.operators.python_operator import ShortCircuitOperator
 from airflow.providers.amazon.aws.operators.s3 import S3ListOperator
 from airflow.models import Variable
-
+from kubernetes.client import models as k8s
 
 # SCRIPTS
 from scripts.hp_externa.mesa_variaveis_v2 import transform_data_to_refined
