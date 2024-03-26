5f753280ae6d29e9ea7d8850a8c4586907de59b0
João Leite <joao.leite@alpe.com.br>
2024-03-26 17:19:13 -0300

    Alterando data para rodar no Mês 02 de 2024

--- dags/organizacoes_receita_federal.py
+++ dags/organizacoes_receita_federal.py
@@ -18,9 +18,9 @@ RECEITA_FEDERAL_ESTABELECIMENTOS_FOLDER = "estabelecimentos/"
 RECEITA_FEDERAL_EMPRESAS_FOLDER = "empresas/"
 
 now = datetime.now(tz=timezone(timedelta(hours=-3)))
-yesterday = now - timedelta(month=1)
-day_to_process_estabelecimentos = f"{RECEITA_FEDERAL_ESTABELECIMENTOS_FOLDER}year={yesterday.year}/month={str(yesterday.month)}/"
-day_to_process_empresas = f"{RECEITA_FEDERAL_EMPRESAS_FOLDER}year={yesterday.year}/month={str(yesterday.month)}/"
+#yesterday = now - timedelta(month=1)
+day_to_process_estabelecimentos = f"{RECEITA_FEDERAL_EMPRESAS_FOLDER}year=2024/month=2/" #f"{RECEITA_FEDERAL_ESTABELECIMENTOS_FOLDER}year={yesterday.year}/month={str(yesterday.month)}/"
+day_to_process_empresas = f"{RECEITA_FEDERAL_EMPRESAS_FOLDER}year=2024/month=2/" #f"{RECEITA_FEDERAL_EMPRESAS_FOLDER}year={yesterday.year}/month={str(yesterday.month)}/"
 
 # DEFINE FUNCTIONS
 def check_files_to_processed(files_to_process):
