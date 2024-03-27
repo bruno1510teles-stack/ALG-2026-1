591103f1738b819e001928540a9aecf852cf2ba0
João Leite <joao.leite@alpe.com.br>
2024-03-27 14:46:26 -0300

    Alterando memoria

--- dags/organizacoes_receita_federal.py
+++ dags/organizacoes_receita_federal.py
@@ -75,7 +75,7 @@ def organizacoes_receita_federal():
     @task(
         executor_config={
         "KubernetesExecutor": {
-            "request_memory": "16000Mi"
+            "request_memory": "12288Mi"
         }
     }
     )
