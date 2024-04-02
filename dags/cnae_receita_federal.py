39947192f3ed67b603d121f06f282989c4559d5f
João Leite <joao.leite@alpe.com.br>
2024-04-02 17:45:35 -0300

    Primeiro teste da tabale a de CNAES

---
+++ dags/cnae_receita_federal.py
@@ -0,0 +1,114 @@
+# IMPORT LIBS
+from datetime import datetime, timedelta, timezone
+
+# AIRFLOW LIBS
+from airflow.decorators import dag, task
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python_operator import ShortCircuitOperator
+from airflow.providers.amazon.aws.operators.s3 import S3ListOperator
+from airflow.models import Variable
+
+# SCRIPTS
+from scripts.receita_federal.cnae_pessoas_e_organizacoes import transform_receita_to_cnae
+
+# DEFINE VARIABLES
+MINIO_CONN_RAW = "minio_raw"
+MINIO_RAW_BUCKET = "receita-federal"
+RECEITA_FEDERAL_ESTABELECIMENTOS_FOLDER = "estabelecimentos/"
+RECEITA_FEDERAL_CNAE_FOLDER = "cnae/"
+
+now = datetime.now(tz=timezone(timedelta(hours=-3)))
+#yesterday = now - timedelta(day=1)
+day_to_process_estabelecimentos = f"{RECEITA_FEDERAL_ESTABELECIMENTOS_FOLDER}year=2024/month=2/" #f"{RECEITA_FEDERAL_ESTABELECIMENTOS_FOLDER}year={yesterday.year}/month={str(yesterday.month)}/"
+day_to_process_cnae = f"{RECEITA_FEDERAL_CNAE_FOLDER}year=2024/month=2/" #f"{RECEITA_FEDERAL_CNAE_FOLDER}year={yesterday.year}/month={str(yesterday.month)}/"
+
+# DEFINE FUNCTIONS
+def check_files_to_processed(files_to_process):
+    print(len(files_to_process))
+    return len(files_to_process) > 0
+
+# DEFINE DEFAULT ARGS
+default_args = {
+    "owner": "João Leite",
+    "retries": 0,
+    "retry_delay": 0,
+    "execution_timeout": timedelta(seconds=60 * 60 * 4),
+}
+
+# DEFINE DAG
+@dag(
+    start_date=datetime(2024, 3, 1), # definir quando for rodar automatico
+    max_active_runs=1,
+    schedule_interval='0 10 * * *',
+    default_args=default_args,
+    catchup=False,
+    tags=['etl', 'minio', 'mesa', 'variaveis', 'motor']
+)
+def cnae_receita_federal():
+    # init & finish task
+    init_data_load = EmptyOperator(task_id="init")
+    finish_data_load = EmptyOperator(task_id="finish")
+
+    list_today_files_estabelecimento = S3ListOperator(
+        task_id="list_today_files_estabelecimento",
+        aws_conn_id=MINIO_CONN_RAW,
+        bucket=MINIO_RAW_BUCKET,
+        prefix=day_to_process_estabelecimentos,
+        apply_wildcard=True,
+    )
+    
+    list_today_files_cnae = S3ListOperator(
+        task_id="list_today_files_cnae",
+        aws_conn_id=MINIO_CONN_RAW,
+        bucket=MINIO_RAW_BUCKET,
+        prefix=day_to_process_cnae,
+        apply_wildcard=True,
+    )
+
+    check_files = ShortCircuitOperator(
+        task_id='check_files',
+        python_callable=check_files_to_processed,
+        provide_context=True,
+        op_kwargs={'files_to_process': list_today_files_estabelecimento.output}
+    )
+    
+    @task(
+        executor_config={
+        "KubernetesExecutor": {
+            "request_memory": "12000Mi"
+        }
+    }
+    )
+    def cnae_receita_federal(current_files_estabelecimento, current_files_cnae):
+
+        access_params = {          
+            "endpoint_url_trusted": Variable.get("MINIO_TRUSTED_ENDPOINT"),
+            "aws_access_key_id_trusted": Variable.get("MINIO_TRUSTED_ACCESS_KEY"),
+            "aws_secret_access_key_trusted": Variable.get("MINIO_TRUSTED_SECRET_KEY"),
+            "endpoint_url_refined": Variable.get("MINIO_REFINED_ENDPOINT"),
+            "aws_access_key_id_refined": Variable.get("MINIO_REFINED_ACCESS_KEY"),
+            "aws_secret_access_key_refined": Variable.get("MINIO_REFINED_SECRET_KEY"),
+            "endpoint_url_raw": Variable.get("MINIO_RAW_ENDPOINT"),
+            "aws_access_key_id_raw": Variable.get("MINIO_RAW_ACCESS_KEY"),
+            "aws_secret_access_key_raw": Variable.get("MINIO_RAW_SECRET_KEY"),
+            "trino_endpoint": Variable.get("TRINO_ENDPOINT"),
+            "trino_port": Variable.get("TRINO_PORT"),
+            "trino_user": Variable.get("TRINO_USER"),
+            "trino_password": Variable.get("TRINO_PASSWORD"),
+            "opdb_bucket": Variable.get("OPDB_BUCKET"),
+            "stage": Variable.get('STAGE')
+	
+
+        }
+
+        print(f"current_files_estabelecimento as { type(current_files_estabelecimento) } and size of { len(current_files_estabelecimento) }")
+        print(f"current_files_cnae as { type(current_files_cnae) } and size of { len(current_files_cnae) }")
+        
+        transform_receita_to_cnae(current_files_estabelecimento, current_files_cnae, access_params)
+
+    unique_clients = cnae_receita_federal(list_today_files_estabelecimento.output, list_today_files_cnae.output)
+
+    # run order
+    init_data_load >> list_today_files_estabelecimento >> check_files >> list_today_files_cnae >> unique_clients >> finish_data_load
+
+cnae_receita_federal()
---
+++ dags/scripts/receita_federal/cnae_pessoas_e_organizacoes.py
@@ -0,0 +1,146 @@
+# Carregando libs
+import pandas as pd
+import pyarrow as pa
+import copy
+from datetime import datetime, timezone, timedelta
+from minio import Minio
+from io import BytesIO
+import os
+from deltalake import write_deltalake, DeltaTable
+import psutil
+import numpy as np
+
+
+# Criando conexão
+def transform_receita_to_cnae(files_list_estabelecimento, files_list_cnae, access_params):
+
+    # Variaveis Conexão
+    BUCKET_SOURCE_RAW = "receita-federal"
+    RAW_ESTABELECIMENTO_FOLDER = "estabelecimentos/"
+    RAW_CNAE_FOLDER = "cnae/"
+    BUCKET_SOURCE_TRUSTED = "pessoas-e-organizacoes"
+    TRUSTED_FOLDER = "cnae/"
+
+    # Conectando na trusted
+    client = Minio(
+        access_params['endpoint_url_raw'],
+        access_key = access_params['aws_access_key_id_raw'],
+        secret_key = access_params['aws_secret_access_key_raw'],
+    )
+    
+    #Como é apenas um arquivo de CNAE por vez não é necessário concatenar DF como os outros tratamentos
+    print('IMPORTANDO CNAES')
+    for file_name in files_list_cnae:
+        print(f"file_name: {file_name}")
+        file = client.get_object(bucket_name=BUCKET_SOURCE_RAW, object_name=file_name)
+        df_cnae = pd.read_csv(BytesIO(file.data), dtype='str', sep=';', header=None, encoding='latin1')
+
+    dict_cnaes = dict(zip(df_cnae[0], df_cnae[1]))
+    
+    # Importando dados de ESTABELECIMENTO
+    print('IMPORTANDO ESTABELECIMENTO')
+
+    #definindo colunas a serem utilizadas
+    colunas_estabelecimentos = [0, 1, 2, 21, 22, 23, 24, 25, 26, 27]
+
+    #definindo tipo das colunas para importacao
+    dtypes_estabelecimentos = {0:'string', 1:'string', 2:'string',
+                            21:'string', 22:'string', 23:'string',
+                            24:'string', 25:'string', 26:'string',
+                            27:'string'}
+
+    
+    #Tratando um arquivo por vez
+    for file_name in files_list_estabelecimento:
+        print(f"file_name: {file_name}")
+
+        file = client.get_object(bucket_name=BUCKET_SOURCE_RAW, object_name=file_name)
+        for df in pd.read_csv(BytesIO(file.data), sep=';', 
+                            encoding='latin1', low_memory=False, chunksize=6000000, dtype=dtypes_estabelecimentos, usecols=colunas_estabelecimentos, header=None):
+        
+            
+            #definindo colunas a serem utilizadas
+            colunas_estabelecimentos = [0, 1, 2, 11, 12]
+
+            #definindo tipo das colunas para importacao
+            dtypes_estabelecimentos = {0:'string', 1:'string', 2:'string', 11:'string', 12:'string'}
+
+            df = pd.read_csv(r'C:\Users\joao.leite\OneDrive - Yandeh\Desktop\receita-federal\estabelecimentos\K3241.K03200Y1.D40210.ESTABELE', sep=';', dtype=dtypes_estabelecimentos, usecols=colunas_estabelecimentos, encoding='latin1', nrows = 2000, low_memory=False, header=None)
+
+            #definindo nome das colunas para tratamento inicial
+            nome_colunas_estabelecimentos = {0:'CNPJ BÁSICO',
+                1:'CNPJ ORDEM',
+                2:'CNPJ DV',
+                11:'CNAE FISCAL PRINCIPAL',
+                12:'CNAE FISCAL SECUNDARIA'}
+
+            df = df.rename(columns=nome_colunas_estabelecimentos)
+
+            #criando coluna de identificador juntando todas as colunas de documento
+            df['identificador'] = df['CNPJ BÁSICO'] + df['CNPJ ORDEM'] + df['CNPJ DV']
+
+            #criando coluna de código cnae
+            df['CODIGO CNAE PRINCIPAL'] = df['CNAE FISCAL PRINCIPAL']
+            df['CODIGO CNAE SECUNDARIA'] = df['CNAE FISCAL SECUNDARIA']
+
+            #separando string de CNEAS secundários em linhas diferentes
+            df['CODIGO CNAE SECUNDARIA'] = df['CODIGO CNAE SECUNDARIA'].str.split(',')
+            df = df.explode('CODIGO CNAE SECUNDARIA')
+
+            #Como o código anterior dupliaca o numero de cnaes principais, ao deixar cada CNAE SECUNDARIA em uma linha, anulamos os cnaes principais duplicados para posterior remoção.
+            df['CODIGO CNAE PRINCIPAL'] = np.where(df.duplicated(subset=['identificador', 'CODIGO CNAE PRINCIPAL']), np.nan, df['CODIGO CNAE PRINCIPAL'])
+
+            #eliminando colunas desnecessárioas após o tratamento
+            df = df.drop(columns=['CNPJ BÁSICO', 'CNPJ ORDEM', 'CNPJ DV', 'CNAE FISCAL PRINCIPAL', 'CNAE FISCAL SECUNDARIA'])
+
+            #deixando um CNAE por linha
+            df = df.melt(id_vars='identificador', value_vars=['CODIGO CNAE PRINCIPAL', 'CODIGO CNAE SECUNDARIA'], 
+                            value_name='codigo')
+
+            #usando dict para renomear os códigos de CNAES
+            df['descricao'] = df['codigo'].map(dict_cnaes)
+
+            df = df.rename(columns={'variable': 'tipo'})
+
+            #ordenando colunas
+            df = df[['identificador', 'codigo', 'descricao', 'tipo']]
+
+            #alterando registro da coluna tipo
+            dict_tipo = {'CODIGO CNAE PRINCIPAL': 'PRIMÁRIO',
+                        'CODIGO CNAE SECUNDARIA': 'SECUNDÁRIO'}
+            df['tipo'] = df['tipo'].map(dict_tipo)
+
+            #eliminando CNAES nulos criados no tratamento de duplicatas
+            df = df.dropna(subset='codigo')
+
+            
+            df['fonte'] = 'RECEITA FEDERAL'
+
+            #Definindo data de tratamento do arquivo
+            now = datetime.now(tz=timezone(timedelta(hours=-3)))
+            
+            df['year'] = now.year
+            df['month'] = now.month
+            df['day'] = now.day
+                
+            storage_options = {
+                "AWS_ACCESS_KEY_ID": access_params['aws_access_key_id_trusted'],
+                "AWS_SECRET_ACCESS_KEY": access_params['aws_secret_access_key_trusted'],
+                "AWS_ENDPOINT_URL":f"https://{access_params['endpoint_url_trusted']}",
+                "AWS_REGION": "us-east-1",
+                "AWS_S3_ALLOW_UNSAFE_RENAME": "true",
+            }
+            
+            print("Transformando em Pyarrow!")
+            # O pandas cria esse index, este codigo serve para remover caso ele crie
+            df = pa.Table.from_pandas(df, preserve_index=False)
+
+            print("Gravando na Trused!")
+            write_deltalake(f"s3a://{BUCKET_SOURCE_TRUSTED}/{TRUSTED_FOLDER}", 
+                            df, 
+                            partition_by=["year", "month", "day"],
+                            storage_options=storage_options,
+                            mode="append",
+                            )
+            
+            print("Gravado na Trused!")
