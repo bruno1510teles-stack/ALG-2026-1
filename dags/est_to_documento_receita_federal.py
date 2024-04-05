b990dfd6faf2271b0cc959562b9ed43cdd404d27
João Leite <joao.leite@alpe.com.br>
2024-04-05 14:10:23 -0300

    Alterando memoria utilizada

--- dags/est_to_documento_receita_federal.py
+++ dags/est_to_documento_receita_federal.py
@@ -66,7 +66,7 @@ def estabelecimento_to_documento_receita_federal():
     @task(
         executor_config={
         "KubernetesExecutor": {
-            "request_memory": "16000Mi"
+            "request_memory": "12000Mi"
         }
     }
     )
