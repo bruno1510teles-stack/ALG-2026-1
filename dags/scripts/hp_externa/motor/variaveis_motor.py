710a5561015860e834a7724f6d3323e5fc9e4a9e
felipe.ferraz <felipe.ferraz@alpe.com.br>
2024-04-17 10:41:20 -0300

    delta.tables.load_table

--- dags/scripts/hp_externa/motor/variaveis_motor.py
+++ dags/scripts/hp_externa/motor/variaveis_motor.py
@@ -10,6 +10,7 @@ from deltalake import write_deltalake, DeltaTable
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType, FloatType
 from delta import *
+import delta
 from scripts.query_trino_payments import query_trino
 
 # Calculando Variáveis
@@ -599,7 +600,7 @@ def transform_data_to_refined(files_list, access_params):
     print("Caminho Delta:", delta_path)
 
     # Carregue os dados do Delta Lake usando pyarrow
-    delta_table = DeltaTable.forPath(delta_path)
+    delta_table = delta.tables.load_table(delta_path)
 
     # # Obtenha o esquema atual da tabela Delta
     # current_schema = delta_table.schema()
