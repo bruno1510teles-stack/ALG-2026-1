e39084666ae5aeaf006e9a4319a6515c77f34ea5
felipe.ferraz <felipe.ferraz@alpe.com.br>
2024-04-17 10:48:38 -0300

    delta.tables.load_as_arrow

--- dags/scripts/hp_externa/motor/variaveis_motor.py
+++ dags/scripts/hp_externa/motor/variaveis_motor.py
@@ -600,7 +600,7 @@ def transform_data_to_refined(files_list, access_params):
     print("Caminho Delta:", delta_path)
 
     # Carregue os dados do Delta Lake usando pyarrow
-    delta_table = delta.tables.load_table(delta_path)
+    delta_table = delta.tables.load_as_arrow(delta_path)
 
     # # Obtenha o esquema atual da tabela Delta
     # current_schema = delta_table.schema()
