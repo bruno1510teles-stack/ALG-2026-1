100ab2383f2aece5e294d5717e3b97aea60f3f28
João Leite <joao.leite@alpe.com.br>
2024-04-25 10:36:25 -0300

    Ajustando erro de EOL na string de processamento dos arquivos

--- dags/resposta_analises_credito.py
+++ dags/resposta_analises_credito.py
@@ -20,11 +20,11 @@ RESPOSTA_MESA_RAW_FOLDER = f"topics/opdb.inrp_prd_default.jira_issue/"
 now = datetime.now(tz=timezone(timedelta(hours=-3)))
 yesterday = now - timedelta(days=1)
 
-day_to_process_motor = f"{RESPOSTA_MOTOR_RAW_FOLDER}year={yesterday.year}
+day_to_process_motor = f"{RESPOSTA_MOTOR_RAW_FOLDER}year={yesterday.year}/"
 
 #/month={str(yesterday.month).zfill(2)}/day={str(yesterday.day).zfill(2)}/"
 
-day_to_process_mesa = f"{RESPOSTA_MESA_RAW_FOLDER}year={yesterday.year}/
+day_to_process_mesa = f"{RESPOSTA_MESA_RAW_FOLDER}year={yesterday.year}/"
 
 # month={str(yesterday.month).zfill(2)}/day={str(yesterday.day).zfill(2)}/"
 
