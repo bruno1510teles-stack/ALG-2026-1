d5ce7d1bc0363e1aee899ca605ff8cdb5cff1f83
João Leite <joao.leite@alpe.com.br>
2024-04-25 15:22:56 -0300

    Testando explicitar tz='-3:00' ao invés de BRT como timezone

--- dags/scripts/respostas_credito/motor/respostas_motor_to_trusted.py
+++ dags/scripts/respostas_credito/motor/respostas_motor_to_trusted.py
@@ -247,7 +247,7 @@ def transform_motor_to_trusted(files_list_motor, access_params):
         ('resolution', pa.string()),
         ('state', pa.string()),
         ('fonte', pa.string()),
-        ('atualizado_em', pa.timestamp('BRT')),
+        ('atualizado_em', pa.timestamp(tz='-3:00')),
         ('year', pa.int32()),
         ('month', pa.int32()),
         ('day', pa.int32())
