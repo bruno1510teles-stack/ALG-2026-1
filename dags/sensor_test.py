c79a5ad222480546842492c00f314f8a85950be6
João Leite <joao.leite@alpe.com.br>
2024-05-07 08:09:00 -0300

    Desenvolvendo sensor de arquivos

---
+++ dags/sensor_test.py
@@ -0,0 +1,70 @@
+# IMPORT LIBS
+from datetime import datetime, timedelta, timezone
+
+# AIRFLOW LIBS
+from airflow.decorators import dag, task
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python_operator import ShortCircuitOperator
+from airflow.providers.amazon.aws.operators.s3 import S3ListOperator
+from airflow.providers.amazon.aws.sensor.s3_key import S3KeySensor
+from airflow.models import Variable
+from minio import Minio
+
+access_params = {          
+    "endpoint_url_trusted": Variable.get("MINIO_TRUSTED_ENDPOINT"),
+    "aws_access_key_id_trusted": Variable.get("MINIO_TRUSTED_ACCESS_KEY"),
+    "aws_secret_access_key_trusted": Variable.get("MINIO_TRUSTED_SECRET_KEY"),
+    "endpoint_url_refined": Variable.get("MINIO_REFINED_ENDPOINT"),
+    "aws_access_key_id_refined": Variable.get("MINIO_REFINED_ACCESS_KEY"),
+    "aws_secret_access_key_refined": Variable.get("MINIO_REFINED_SECRET_KEY"),
+    "endpoint_url_raw": Variable.get("MINIO_RAW_ENDPOINT"),
+    "aws_access_key_id_raw": Variable.get("MINIO_RAW_ACCESS_KEY"),
+    "aws_secret_access_key_raw": Variable.get("MINIO_RAW_SECRET_KEY"),
+    "trino_endpoint": Variable.get("TRINO_ENDPOINT"),
+    "trino_port": Variable.get("TRINO_PORT"),
+    "trino_user": Variable.get("TRINO_USER"),
+    "trino_password": Variable.get("TRINO_PASSWORD"),
+    "opdb_bucket": Variable.get("OPDB_BUCKET"),
+    "stage": Variable.get('STAGE')
+
+
+}
+# DEFINE DEFAULT ARGS
+default_args = {
+    "owner": "João Leite",
+    "retries": 0,
+    "retry_delay": 0,
+    "execution_timeout": timedelta(seconds=60 * 60 * 4),
+}
+
+@dag(
+    start_date=datetime(2024, 3, 1), # definir quando for rodar automatico
+    max_active_runs=1,
+    schedule_interval='0 10 * * *',
+    default_args=default_args,
+    catchup=False,
+    tags=['etl', 'minio', 'mesa', 'variaveis', 'motor']
+)
+def sensor_test():
+    # # Variaveis Conexão
+    # BUCKET_SOURCE_RAW = "receita-federal"
+    # BUCKET_SOURCE_TRUSTED = "pessoas-e-organizacoes"
+    # TRUSTED_FOLDER = "relacionamento/"
+
+    # Conectando na trusted
+    client = Minio(
+        access_params['endpoint_url_raw'],
+        access_key = access_params['aws_access_key_id_raw'],
+        secret_key = access_params['aws_secret_access_key_raw'],
+    )
+    task1 = S3KeySensor(
+        task_id='sensor_minio_s3',
+        bucket_name='teste-vini',
+        bucket_key='data.csv',
+        aws_conn_id=client
+    
+    )
+    
+    task1 
+
+sensor_test()
