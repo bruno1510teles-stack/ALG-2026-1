ff969d92092a70c1558cfb8030d2194f12d23df4
João Leite <joao.leite@alpe.com.br>
2024-06-27 16:47:24 -0300

    Teste dag print

---
+++ dags/scripts/receita_federal/receita_to_raw/receita_to_raw.py
@@ -0,0 +1,17 @@
+# Carregando libs
+import pandas as pd
+import pyarrow as pa
+import copy
+from datetime import datetime, timezone, timedelta
+from minio import Minio
+from io import BytesIO
+import os
+from deltalake import write_deltalake, DeltaTable
+import psutil
+import requests
+
+# Criando conexão
+
+def receita_to_raw():
+    print(os.getcwd())
+    print(os.listdir(os.getcwd()))
