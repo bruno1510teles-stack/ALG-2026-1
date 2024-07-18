55af601ebb142c606ef9a97f110893298a134bbc
João Leite <joao.leite@alpe.com.br>
2024-07-18 09:56:55 -0300

    Debugando timeout

--- dags/analises_credito/execucao_motor.py
+++ dags/analises_credito/execucao_motor.py
@@ -58,9 +58,14 @@ def execucao_motor():
     finish_data_load = EmptyOperator(task_id="finish")
     
 
-    pre_filtro = analise_pre_filtro(access_params)
+    @task(executor_config={"KubernetesExecutor": {"request_memory": "12000Mi"}})
+    def pre_filtro(access_params):
+        return analise_pre_filtro(access_params)
 
-    modelo = execucao_modelo(access_params)
+    @task(executor_config={"KubernetesExecutor": {"request_memory": "12000Mi"}})
+    def modelo(access_params):
+        return execucao_modelo(access_params)
+
      
     # @task(executor_config={"KubernetesExecutor": {"request_memory": "12000Mi"}})
     # def politica(access_params):
