a5f67c5f89f6117aef4d9bd0d99b04cbd3dcd277
João Leite <joao.leite@alpe.com.br>
2024-07-24 14:13:46 -0300

    Testando ajuste de filtro pf6

--- dags/scripts/analises_credito/motor/execucao_analise_pre_filtro.py
+++ dags/scripts/analises_credito/motor/execucao_analise_pre_filtro.py
@@ -246,7 +246,7 @@ def analise_pre_filtro(access_params=None):
     # PF 5
     df.loc[(df['nat_ju_aceita'] == 'NAO') & (df['ramificacao_pre_filtro'].isna()), 'ramificacao_pre_filtro'] = 'PF 5'
     # PF 6
-    df.loc[(df['idade'] < 2) & (df['ramificacao_pre_filtro'].isna()), 'ramificacao_pre_filtro'] == 'PF 6'
+    df.loc[((df['idade'] < 2) & (df['ramificacao_pre_filtro'].isna())), 'ramificacao_pre_filtro'] == 'PF 6'
     # PF 7
     df.loc[((df['tem_pep'] == 'True') | (df['tem_pep'] == True)) & (df['ramificacao_pre_filtro'].isna()), 'ramificacao_pre_filtro'] = 'PF 7'
     # PF 8
