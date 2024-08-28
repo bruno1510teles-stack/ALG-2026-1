5fb49ffcbe5c1d908aae2c992a8aa8b883e0ddd2
felipe.ferraz <felipe.ferraz@alpe.com.br>
2024-08-28 17:23:47 -0300

    colocando intervalo de 30 min para executar

--- dags/scripts/analises_credito/motor/execucao_motor.py
+++ dags/scripts/analises_credito/motor/execucao_motor.py
@@ -57,7 +57,7 @@ with DAG(
     dag_id="execucao_motor",
     start_date=datetime(2024, 3, 1),
     max_active_runs=1,
-    schedule_interval='0 18 * * *',
+    schedule_interval='*/30 * * * *',
     default_args=default_args,
     catchup=False,
     tags=['etl', 'minio', 'mesa', 'variaveis', 'motor']
