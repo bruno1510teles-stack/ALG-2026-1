07d2a5e6b87165cc9abaffc2878b89a3c8658e7d
Matheus Magalhães Barbosa <matheus.barbosa@alpenet.com.br>
2024-08-30 17:08:15 -0300

    Formata arquivo

--- dags/anexa_grupo_economico/organograma.py
+++ dags/anexa_grupo_economico/organograma.py
@@ -8,65 +8,65 @@ from minio.error import S3Error
 from airflow_dags_core.lib.MinioWriteFile import MinioWriteFile
 from airflow_dags_core.lib.MinioReadFile import MinioReadFile
 
-default_args = {
-    'owner': 'Matheus Barbosa'
-}
+default_args = {"owner": "Matheus Barbosa"}
+
 
 @dag(
     default_args=default_args,
-    start_date=pendulum.today('UTC').add(days=-1),
-    schedule= None,
-    description='DAG consulta trino e faz upload do arquivo para minio',
+    start_date=pendulum.today("UTC").add(days=-1),
+    schedule=None,
+    description="DAG consulta trino e faz upload do arquivo para minio",
     catchup=False,
 )
 def anexa_grupo_economico():
 
     @task
     def consulta_trino_e_anexa_arquivo(**kwargs):
-            cnpjRaiz = kwargs['dag_run'].conf.get('payer_identification', 'default_value')
-            bucket = kwargs['dag_run'].conf.get('minio_url', 'default_value')
-            
-            trino_endpoint = Variable.get("TRINO_ENDPOINT")
-            trino_port = Variable.get("TRINO_PORT")
-            trino_username = Variable.get("TRINO_USER")
-            trino_password = Variable.get("TRINO_PASSWORD") 
+        cnpjRaiz = kwargs["dag_run"].conf.get("payer_identification", "default_value")
+        bucket = kwargs["dag_run"].conf.get("minio_url", "default_value")
 
-            query = f"select cnpj_raiz, 'catalogo/'||arquivo arquivo from minioraw.credito_grupos.indice where cnpj_raiz = '{cnpjRaiz}'"
+        trino_endpoint = Variable.get("TRINO_ENDPOINT")
+        trino_port = Variable.get("TRINO_PORT")
+        trino_username = Variable.get("TRINO_USER")
+        trino_password = Variable.get("TRINO_PASSWORD")
 
-            retorno = consulta_trino(query=query, host=trino_endpoint, port=trino_port, user=trino_username, password=trino_password)
+        query = f"select cnpj_raiz, 'catalogo/'||arquivo arquivo from minioraw.credito_grupos.indice where cnpj_raiz = '{cnpjRaiz}'"
 
-            data = retorno[0][1]
+        retorno = consulta_trino(
+            query=query,
+            host=trino_endpoint,
+            port=trino_port,
+            user=trino_username,
+            password=trino_password,
+        )
 
-            arquivo = baixa_arquivo_minio(file_name=data, bucket=bucket)
+        data = retorno[0][1]
 
-            pdf_content = BytesIO(arquivo.read())
+        arquivo = baixa_arquivo_minio(file_name=data, bucket=bucket)
 
-            MinioWriteFile().write_file(file=pdf_content, 
-                                    file_name=data, 
-                                    bucket=bucket)
+        pdf_content = BytesIO(arquivo.read())
+
+        MinioWriteFile().write_file(file=pdf_content, file_name=data, bucket=bucket)
 
     def baixa_arquivo_minio(file_name, bucket):
-            try:
-                return MinioReadFile().read_file(file_name=file_name,
-                                        bucket=bucket)    
-            except S3Error as e:
-                print(f"Erro ao interagir com o MinIO: {e}")
-                raise
-            except Exception as e:
-                print(f"Erro inesperado: {e}") 
-                raise
-        
+        try:
+            return MinioReadFile().read_file(file_name=file_name, bucket=bucket)
+        except S3Error as e:
+            print(f"Erro ao interagir com o MinIO: {e}")
+            raise
+        except Exception as e:
+            print(f"Erro inesperado: {e}")
+            raise
+
     def consulta_trino(query, host, port, user, password):
         try:
             conn = trino.dbapi.connect(
                 host=host,
                 port=port,
                 user=user,
-                catalog='minioraw',
-                http_scheme='https',
-                auth=trino.auth.BasicAuthentication(
-                    user, 
-                    password),
+                catalog="minioraw",
+                http_scheme="https",
+                auth=trino.auth.BasicAuthentication(user, password),
             )
 
             cursor = conn.cursor()
@@ -78,12 +78,13 @@ def anexa_grupo_economico():
             print(f"Erro ao executar a consulta: {e}")
             raise
         finally:
-            conn.close()    
+            conn.close()
 
     start = EmptyOperator(task_id="start")
     relato = consulta_trino_e_anexa_arquivo()
     end = EmptyOperator(task_id="end")
 
     start >> relato >> end
+
 
 dag_instance = anexa_grupo_economico()
