ac2f469836fc40d941857562ecac86cadc7d2848
felipe.ferraz <felipe.ferraz@alpe.com.br>
2024-09-02 13:49:16 -0300

    colocando url no path

--- dags/scripts/analises_credito/motor/execucao_politica.py
+++ dags/scripts/analises_credito/motor/execucao_politica.py
@@ -599,7 +599,7 @@ def execucao_politica(access_params=None):
     # Gerando Path
     def gerando_path(row):
            current_date = datetime.now().strftime('%Y-%m-%d')
-           return f"{row['cnpj_raiz']}/{current_date}-{row['pgid']}-{row['issue_jira']}/arquivos"
+           return f"https://minio-datalake.alpe.com.br/raw/browser/{row['cnpj_raiz']}/{current_date}-{row['pgid']}-{row['issue_jira']}/arquivos/"
     # Aplicando o Path
     resposta_motor['path_arquivos_minio'] = resposta_motor.apply(gerando_path, axis = 1)
 
