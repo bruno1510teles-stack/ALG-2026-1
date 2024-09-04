56da98eb8c92f85fa7d17186a11e7fb027a3905a
Rafael Leite <rafael.leite@itlab.com.br>
2024-09-04 14:27:48 -0300

    wip

--- dags/scripts/utils.py
+++ dags/scripts/utils.py
@@ -1,7 +1,6 @@
 from urllib.parse import urlparse
 
 def extract_path_from_url(url):
-    url = "https://minio-datalake.alpe.com.br/raw/browser/analise-credito/47074079/2024-09-03-arcelor-CMGT-26864/arquivos/"
     parsed_url = urlparse(url)
     extracted_path = parsed_url.path.replace("/raw/browser/", "")
     return extracted_path
