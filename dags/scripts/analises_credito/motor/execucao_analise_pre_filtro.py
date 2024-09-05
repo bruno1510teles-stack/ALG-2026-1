7c57e617c919f5a054ad63adc5f56cf5afebcdc0
felipe.ferraz <felipe.ferraz@alpe.com.br>
2024-09-05 12:05:02 -0300

    ajustando pre_filtro

--- dags/scripts/analises_credito/motor/execucao_analise_pre_filtro.py
+++ dags/scripts/analises_credito/motor/execucao_analise_pre_filtro.py
@@ -201,6 +201,10 @@ def analise_pre_filtro(access_params=None):
     df.loc[df['situacao_cadastral'] != 'ATIVA', 'ramificacao_pre_filtro'] = 'PF 1'
     # PF 2
     df.loc[(df['situacao_especial'] == 'RECUPERACAO JUDICIAL') & (df['ramificacao_pre_filtro'].isna()), 'ramificacao_pre_filtro'] = 'PF 2'
+    # PF 7
+    df.loc[((df['tem_pep'] == 'True') | (df['tem_pep'] == True)) & (df['ramificacao_pre_filtro'].isna()), 'ramificacao_pre_filtro'] = 'PF 7'
+    # PF 11
+    df.loc[((df['limite_alpe'] == True) | (df['limite_alpe'] == 'True')) & (df['ramificacao_pre_filtro'].isna()), 'ramificacao_pre_filtro'] = 'PF 11'
     # PF 3
     df.loc[((df['is_mei'] == True) | (df['is_mei'] == 'True')) & (df['ramificacao_pre_filtro'].isna()), 'ramificacao_pre_filtro'] = 'PF 3'
     # PF 4
@@ -209,16 +213,12 @@ def analise_pre_filtro(access_params=None):
     df.loc[(df['nat_ju_aceita'] == 'NAO') & (df['ramificacao_pre_filtro'].isna()), 'ramificacao_pre_filtro'] = 'PF 5'
     # PF 6
     df.loc[(df['is_spe_consorcio_construtora']) & (df['ramificacao_pre_filtro'].isna()), 'ramificacao_pre_filtro'] = 'PF 6'
-    # PF 7
-    df.loc[((df['tem_pep'] == 'True') | (df['tem_pep'] == True)) & (df['ramificacao_pre_filtro'].isna()), 'ramificacao_pre_filtro'] = 'PF 7'
     # PF 8
     df.loc[(((df['idade_socio'].notna()) & (df['idade_socio'] < 2)) | (df['tem_socio_pj'] == True)) & (df['ramificacao_pre_filtro'].isna()), 'ramificacao_pre_filtro'] = 'PF 8'
     # PF 9
     df.loc[((df['idade'] < 2) & (df['ramificacao_pre_filtro'].isna())), 'ramificacao_pre_filtro'] = 'PF 9'
     # PF 10
     df.loc[(df['inad_alpe'] == 'SIM') & (df['ramificacao_pre_filtro'].isna()), 'ramificacao_pre_filtro'] = 'PF 10'
-    # PF 11
-    df.loc[((df['limite_alpe'] == True) | (df['limite_alpe'] == 'True')) & (df['ramificacao_pre_filtro'].isna()), 'ramificacao_pre_filtro'] = 'PF 11'
     # PF 12
     df.loc[df['ramificacao_pre_filtro'].isna(), 'ramificacao_pre_filtro'] = 'PF 12'
  
