0371580f90048de5f1324dd755d235b676c28ac8
Rafael Leite <rafael.leite@itlab.com.br>
2024-09-19 15:19:33 -0300

    add auth trino  api

---
+++ dags/airflow_dags_core/lib/auth_trino.py
@@ -0,0 +1,8 @@
+from requests.auth import HTTPBasicAuth
+from airflow.models import Variable
+
+def get_acess_token():
+    username = Variable.get("TRINO_USER")
+    password = Variable.get("TRINO_ACCESS_KEY")  
+
+    return HTTPBasicAuth(username, password)
