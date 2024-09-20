186a0d340abffd41180d3883093324995ebbc51e
Rafael Leite <rafael.leite@itlab.com.br>
2024-09-20 14:22:11 -0300

    fix basic auth organograma

--- dags/airflow_dags_core/lib/auth_trino.py
+++ dags/airflow_dags_core/lib/auth_trino.py
@@ -3,6 +3,6 @@ from airflow.models import Variable
 
 def get_acess_token():
     username = Variable.get("TRINO_USER")
-    password = Variable.get("TRINO_ACCESS_KEY")  
+    password = Variable.get("TRINO_PASSWORD")  
 
     return HTTPBasicAuth(username, password)
