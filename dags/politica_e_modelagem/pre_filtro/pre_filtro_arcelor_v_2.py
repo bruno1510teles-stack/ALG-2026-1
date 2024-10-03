c0cd8274c54b91a05938e17a2c7751166a392648
felipe.ferraz <felipe.ferraz@alpe.com.br>
2024-10-03 09:38:19 -0300

    ajustando chamada serasa v1

--- dags/politica_e_modelagem/pre_filtro/pre_filtro_arcelor_v_2.py
+++ dags/politica_e_modelagem/pre_filtro/pre_filtro_arcelor_v_2.py
@@ -97,7 +97,7 @@ def analise_pre_filtro(access_params=None,  **kwargs):
                 for ticket in tickets:
                     # Acessando o assignee corretamente dentro de fields
                     assignee = ticket['fields'].get('assignee', {}).get('displayName', 'Não atribuído')
-                    decisao = ticket['fields'].get('resolution')
+                    decisao = ticket['fields'].get('resolution', {}).get('name', 'NF')
 
                     # Adiciona ticket à lista all_tickets
                     all_tickets.append(ticket)
