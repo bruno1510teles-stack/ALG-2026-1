700b5a2199bcb2fa2548d3b5d2d449fc65ec14cb
felipe.ferraz <felipe.ferraz@alpe.com.br>
2024-10-09 14:13:21 -0300

    ajuste politica pre_filtro

--- dags/politica_e_modelagem/pre_filtro/pre_filtro_v_2.py
+++ dags/politica_e_modelagem/pre_filtro/pre_filtro_v_2.py
@@ -356,8 +356,8 @@ def analise_pre_filtro(access_params=None,  **kwargs):
     # Criando Resposta
     response_map = {
         'REPROVADO': ['PF CNPJ IRREGULAR', 'PF REPROVA < 60 DIAS', 'PF RJ', 'PF PEP', 'PF MEI', 'PF CNAE', 'PF NATUREZA JURIDICA', 'PF FUNDACAO < 2 ANOS'],
-        'mantido' : ['PF LIMITE SOLICITADO <= ATUAL', 'PF - UTILIZAÇÃO DE LIMITE MÍNIMA NÃO ATINGIDA', 'PF BLOQUEIO ALPE'],
-        'MESA': ['PF MESA', 'PF CONSORCIO/CONSTRUTORA/SPE', 'PF SOCIO PJ OU < 2 ANOS'],
+        'mantido' : ['PF LIMITE SOLICITADO <= ATUAL', 'PF - UTILIZAÇÃO DE LIMITE MÍNIMA NÃO ATINGIDA'],
+        'MESA': ['PF MESA', 'PF CONSORCIO/CONSTRUTORA/SPE', 'PF SOCIO PJ OU < 2 ANOS', 'PF BLOQUEIO ALPE'],
         'SEGUE': ['PF SEGUE']
     }
     # Aplicar as respostas
