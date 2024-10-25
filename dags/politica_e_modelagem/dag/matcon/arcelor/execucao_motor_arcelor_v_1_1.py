7b26f3b754e09a39138907e60a7752bfff08522c
felipe.ferraz <felipe.ferraz@alpe.com.br>
2024-10-25 18:08:45 -0300

    deixando mensagem padrão

--- dags/politica_e_modelagem/dag/matcon/arcelor/execucao_motor_arcelor_v_1_1.py
+++ dags/politica_e_modelagem/dag/matcon/arcelor/execucao_motor_arcelor_v_1_1.py
@@ -44,17 +44,10 @@ access_params = {
     }
 
 def notificar_falha_teams(context):
-    task_id = context['task_instance'].task_id
-
     url = "https://yandehbr.webhook.office.com/webhookb2/3efc9ab8-aba8-4150-8e68-864d086592a3@fe284b6f-c6d2-4028-badb-7d0c22aef0ae/IncomingWebhook/2bb511bca72643d58ea858c433be3aec/e3ad1a1a-7716-40ee-ab81-0f05650df5dc/V2AAjaUAPO15qUofSpSzGh6PW4gkg2FJypyvorUwW89eU1"
-
-    # Verifica se a task_id é 'captura_proposta' e, se for, não envia notificação
-    # if task_id == "captura_proposta":
-    #     return  # Não envia notificação para 'captura_proposta'
-
     mensagem = {
         "title": "Falha na DAG - politica_v_2",
-        "text": f"Falha na DAG: {context['task_instance'].dag_id} na task: {context['task_instance'].task_id} VERIFICAR URGENTE!!"
+        "text": f"Falha na DAG - politica_v_2: {context['task_instance'].dag_id} na task: {context['task_instance'].task_id} verificar URGENTE!!"
     }
     requests.post(url, json=mensagem)
 
