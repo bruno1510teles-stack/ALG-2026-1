830ec11984fc0705d4b6b4659246c6c3e5f9eb21
kevin.cardoso <kevin.cardoso@alpe.com.br>
2024-10-29 15:53:59 -0300

    ajuste end point

--- dags/jira/propostas/extracao_jira_raw.py
+++ dags/jira/propostas/extracao_jira_raw.py
@@ -124,13 +124,9 @@ def base_details_raw(access_params=None):
 
     # Conectando na raw
     client = Minio(
-        #access_params['endpoint_url_raw'],
-        "api-raw.internal.alpe.tech",
-        #access_key=access_params['aws_access_key_id_raw'],
-        access_key="ijq0S5O8Od7RrUl4Q8pd",
-        #secret_key=access_params['aws_secret_access_key_raw'],
-        secret_key="dLjmQCS4txGFgIibG8tt5L4jYoPTdkCt1m4KgCXf"
-        #secure=True
+        access_params['endpoint_url_raw'],
+        access_key=access_params['aws_access_key_id_raw'],
+        secret_key=access_params['aws_secret_access_key_raw']
         )
 
 
