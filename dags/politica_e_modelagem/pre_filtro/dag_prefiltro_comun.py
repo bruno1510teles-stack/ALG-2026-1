e84917cf4024a32698b8961fedb79f06ec38d224
Augusto de Abreu <augusto.abreu@alpenet.com.br>
2024-10-29 11:24:08 -0300

    Criado a dag do prefiltro comum para todas as propostas de negócio

---
+++ dags/politica_e_modelagem/pre_filtro/dag_prefiltro_comun.py
@@ -0,0 +1,64 @@
+from airflow.utils.dates import days_ago
+
+from airflow.decorators import dag, task
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+from airflow.models import Variable
+
+from politica_e_modelagem.pre_filtro.pre_filtro_v_2 import analise_pre_filtro
+
+### Parâmetros de acesso
+access_params = {          
+    "endpoint_url_trusted": Variable.get("MINIO_TRUSTED_ENDPOINT"),
+    "aws_access_key_id_trusted": Variable.get("MINIO_TRUSTED_ACCESS_KEY"),
+    "aws_secret_access_key_trusted": Variable.get("MINIO_TRUSTED_SECRET_KEY"),
+    "endpoint_url_refined": Variable.get("MINIO_REFINED_ENDPOINT"),
+    "aws_access_key_id_refined": Variable.get("MINIO_REFINED_ACCESS_KEY"),
+    "aws_secret_access_key_refined": Variable.get("MINIO_REFINED_SECRET_KEY"),
+    "endpoint_url_raw": Variable.get("MINIO_RAW_ENDPOINT"),
+    "aws_access_key_id_raw": Variable.get("MINIO_RAW_ACCESS_KEY"),
+    "aws_secret_access_key_raw": Variable.get("MINIO_RAW_SECRET_KEY"),
+    "trino_endpoint": Variable.get("TRINO_ENDPOINT"),
+    "trino_port": Variable.get("TRINO_PORT"),
+    "trino_user": Variable.get("TRINO_USER"),
+    "trino_password": Variable.get("TRINO_PASSWORD"),
+    "opdb_bucket": Variable.get("OPDB_BUCKET"),
+    "stage": Variable.get('STAGE'),
+    "kafka_url": Variable.get('KAFKA_DATALAKE_ENDPOINT'),
+    "exrep_url": Variable.get('EXREP_BASE_URL'),
+    "exrep_client_id": Variable.get('EXREP_CLIENT_ID'),
+    "exrep_client_secret": Variable.get('EXREP_CLIENT_SECRET'),
+    "keycloack_token_url": Variable.get('KEYCLOAK_TOKEN_URL')
+    }
+
+### Definindo defaults
+default_args = {
+    "owner": "Felipe Ferraz",
+    "retries": 0,
+}
+
+# Definindo a DAG
+@dag(
+    dag_id='pre_filtro_comun',
+    start_date=days_ago(1),
+    schedule_interval=None,
+    default_args=default_args,
+    catchup=False,
+    tags=['pre-filtro', 'proposta-negocio'],
+    description="Pre filtro das propostas de negócio independente da política",
+    max_active_tasks=1
+)
+def prefiltro_dag():
+    
+    # init & finish task
+    init_data_load = EmptyOperator(task_id="init")
+    finish_data_load = EmptyOperator(task_id="finish")
+
+    prefiltro_task =  PythonOperator(
+        task_id="pre_filtro_comun_task",
+        python_callable=analise_pre_filtro,
+        op_kwargs={'access_params': access_params},
+    )
+
+    # Definindo a ordem de execução das tasks
+    init_data_load >> prefiltro_task >> finish_data_load
