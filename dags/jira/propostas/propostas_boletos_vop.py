c38be9f407087cc8c112e993518268bcc9548a4d
kevin.cardoso <kevin.cardoso@alpe.com.br>
2024-11-27 15:55:36 -0300

    ajuste str5

--- dags/jira/propostas/propostas_boletos_vop.py
+++ dags/jira/propostas/propostas_boletos_vop.py
@@ -97,6 +97,7 @@ def merge_propostas_boletos(access_params=None, **kwargs):
     vop_vendermais = vop_vendermais.groupby('cnpj_sacado_raiz')[colunas_numericas].sum().reset_index()
 
     # Garantindo que 'pgid' e outras colunas de texto sejam tratadas como strings sem qualquer conversão para numérico
+    df_propostas['pgid'] = df_propostas['pgid'].astype(str).str.upper()
     df_propostas['politica_desc'] = df_propostas['politica_desc'].astype(str)
     df_propostas['tipo_proposta'] = df_propostas['tipo_proposta'].astype(str)
     df_propostas['ramificacao_motor_desc'] = df_propostas['ramificacao_motor_desc'].astype(str)
@@ -107,7 +108,7 @@ def merge_propostas_boletos(access_params=None, **kwargs):
     # Filtrando os dados de propostas com 'status_decisao' = 'Aprovado' e 'limite_aprovado' > 0
     propostas = df_propostas.query("status_decisao == 'Aprovado' and limite_aprovado > 0")[
         ['nome_decisor', 'cnpj_sacado_raiz', 'data_criado', 'hora_criado', 
-         'politica_desc', 'tipo_proposta', 'ramificacao_motor_desc']
+         'pgid', 'politica_desc', 'tipo_proposta', 'ramificacao_motor_desc']
     ].reset_index(drop=True)
 
     # Criando a coluna 'data_hora_decisao' combinando as colunas 'data_criado' e 'hora_criado'
@@ -138,7 +139,7 @@ def merge_propostas_boletos(access_params=None, **kwargs):
     # Setando valores 0 nas colunas relevantes quando 'flag_decisor' não for 1
     base_final.loc[
         base_final["flag_decisor"] != 1, 
-        base_final.columns.difference(["flag_decisor", "cnpj_sacado_raiz", "nome_decisor", "pgid", "politica_desc", "tipo_proposta", "ramificacao_motor_desc"])
+        base_final.columns.difference(["flag_decisor", "cnpj_sacado_raiz", "nome_decisor"])
     ] = 0
 
     # Preenchendo valores nulos com 0
