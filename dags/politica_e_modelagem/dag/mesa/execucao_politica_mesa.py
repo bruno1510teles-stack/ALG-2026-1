d2f1b83ad2d6bfe0e2e227cbfacde6ea91fc2d4d
felipe.ferraz <felipe.ferraz@alpe.com.br>
2024-12-04 09:44:42 -0300

    subindo política mesa

---
+++ dags/politica_e_modelagem/dag/mesa/execucao_politica_mesa.py
@@ -0,0 +1,131 @@
+### Importando Libs necessárias
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+from airflow.models import Variable
+import pandas as pd
+from time import sleep
+
+
+### Importando scripts necessários
+from politica_e_modelagem.auxiliares.serasa import execucao_chamada_serasa_v1
+from politica_e_modelagem.politica.mesa import politica_mesa
+from politica_e_modelagem.auxiliares.kafka import execucao_envio_kafka
+
+
+### Parâmetros de acesso
+access_params = {          
+    "endpoint_url_trusted": Variable.get("MINIO_TRUSTED_ENDPOINT"),
+    "aws_access_key_id_trusted": Variable.get("MINIO_TRUSTED_ACCESS_KEY"),
+    "aws_secret_access_key_trusted": Variable.get("MINIO_TRUSTED_SECRET_KEY"),
+    "endpoint_url_refined": Variable.get("MINIO_REFINED_ENDPOINT"),
+    "aws_access_key_id_refined": Variable.get("MINIO_REFINED_ACCESS_KEY"),
+    "aws_secret_access_key_refined": Variable.get("MINIO_REFINED_SECRET_KEY"),
+    "endpoint_url_raw": Variable.get("MINIO_RAW_ENDPOINT"),
+    "aws_access_key_id_raw": Variable.get("MINIO_RAW_ACCESS_KEY"),
+    "aws_secret_access_key_raw": Variable.get("MINIO_RAW_SECRET_KEY"),
+    "trino_endpoint": Variable.get("TRINO_ENDPOINT"),
+    "trino_port": Variable.get("TRINO_PORT"),
+    "trino_user": Variable.get("TRINO_USER"),
+    "trino_password": Variable.get("TRINO_PASSWORD"),
+    "opdb_bucket": Variable.get("OPDB_BUCKET"),
+    "stage": Variable.get('STAGE'),
+    "kafka_url": Variable.get('KAFKA_DATALAKE_ENDPOINT'),
+    "exrep_url": Variable.get('EXREP_BASE_URL'),
+    "exrep_client_id": Variable.get('EXREP_CLIENT_ID'),
+    "exrep_client_secret": Variable.get('EXREP_CLIENT_SECRET'),
+    "keycloack_token_url": Variable.get('KEYCLOAK_TOKEN_URL')
+    }
+
+
+
+### Definindo defaults
+default_args = {
+    "owner": "Felipe Ferraz",
+    "retries": 2,
+}
+
+
+# Função para processar as propostas recebidas
+def processar_proposta(**kwargs):
+    # Capturando os parâmetros enviados via conf
+    conf = kwargs.get('dag_run').conf
+    issue_jira = conf.get('issue_key')
+    CNPJ = conf.get('payer_identification')
+    inad_alpe = conf.get('inad')
+    pgid = conf.get('payee_pgid')
+    nome_issue = conf.get('summary')
+
+    # Criando um dicionário com os dados recebidos para simular o DataFrame
+    dados = {
+        'issue_jira' : [issue_jira],
+        'CNPJ': [CNPJ],
+        'inad_alpe': [inad_alpe],
+        'pgid': [pgid],
+        'nome_issue': [nome_issue]
+    }
+
+    # Convertendo o dicionário em DataFrame para aplicar as transformações
+    df = pd.DataFrame(dados)
+
+    # Passo 1: Remover a máscara de valor e converter para numérico
+    if df['inad_alpe'].notna().any():
+        df['inad_alpe'] = df['inad_alpe'].replace({'R\$ ': '', '\.': ''}, regex=True)
+        df['inad_alpe'] = pd.to_numeric(df['inad_alpe'], errors='coerce')  # Converte para float, substituindo erros por NaN
+
+    # Passo 3: Criar as colunas de flag com base na lógica fornecida
+    df['inad_alpe_flag'] = df['inad_alpe'].apply(lambda x: 'SIM' if pd.notna(x) and x > 0 else 'NAO')
+
+    # Passo 4: Deixar o nome padrão
+    # Remover as colunas originais 'limite_alpe' e 'inad_alpe'
+    df.drop(columns=['inad_alpe'], inplace=True)
+
+    # Renomear as colunas de flag para os nomes originais
+    df.rename(columns={'inad_alpe_flag': 'inad_alpe'}, inplace=True)
+
+    return df.to_dict()
+
+# Definindo a DAG
+with DAG(
+    dag_id='politica_mesa',
+    start_date=days_ago(1),
+    schedule_interval=None,
+    default_args=default_args,
+    tags=['politica_mesa']  # DAG só será acionada manualmente pela API
+) as dag:
+
+    # Definindo o task que processa a proposta
+    captura_proposta = PythonOperator(
+        task_id='captura_proposta',
+        python_callable=processar_proposta,
+        provide_context=True  # Habilita o envio do contexto (incluindo conf)
+    )
+
+
+    # Definindo o task que faz a chamada do serasa
+    serasa = PythonOperator(
+        task_id="serasa_task",
+        python_callable=execucao_chamada_serasa_v1.chamando_serasa,
+        op_kwargs={'access_params': access_params},
+        provide_context=True
+    )
+
+    # Execução da política
+    politica = PythonOperator(
+        task_id="politica_task",
+        python_callable=politica_mesa.execucao_politica,
+        op_kwargs={'access_params': access_params},
+        provide_context=True
+    )
+
+    # Enviando dados para o Kafka
+    enviar_kafka = PythonOperator(
+        task_id="envio_kafka_task",
+        python_callable=execucao_envio_kafka.envio_kafka,
+        op_kwargs={'access_params': access_params},
+        provide_context=True,
+    )
+
+
+    # Definindo a ordem de execução das tasks
+    captura_proposta >> serasa >> politica >> enviar_kafka
