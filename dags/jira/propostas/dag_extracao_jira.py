080286e2732c3fa461b0451cf7531ac13d6e6d81
kevin.cardoso <kevin.cardoso@alpe.com.br>
2024-12-05 14:21:36 -0300

    notif falha

--- dags/jira/propostas/dag_extracao_jira.py
+++ dags/jira/propostas/dag_extracao_jira.py
@@ -45,13 +45,13 @@ access_params = {
     "keycloack_token_url": Variable.get('KEYCLOAK_TOKEN_URL')
     }
 
-# def notificar_falha_teams(context):
-#     url = "https://yandehbr.webhook.office.com/webhookb2/3efc9ab8-aba8-4150-8e68-864d086592a3@fe284b6f-c6d2-4028-badb-7d0c22aef0ae/IncomingWebhook/2bb511bca72643d58ea858c433be3aec/e3ad1a1a-7716-40ee-ab81-0f05650df5dc/V2AAjaUAPO15qUofSpSzGh6PW4gkg2FJypyvorUwW89eU1"
-#     mensagem = {
-#         "title": f"Falha na Execução DAG - {context['task_instance'].dag_id}",
-#         "text": f"Falha na DAG: {context['task_instance'].dag_id} na task: {context['task_instance'].task_id} VERIFICAR URGENTE!!"
-#     }
-#     requests.post(url, json=mensagem)
+def notificar_falha_teams(context):
+    url = "https://yandehbr.webhook.office.com/webhookb2/3efc9ab8-aba8-4150-8e68-864d086592a3@fe284b6f-c6d2-4028-badb-7d0c22aef0ae/IncomingWebhook/2bb511bca72643d58ea858c433be3aec/e3ad1a1a-7716-40ee-ab81-0f05650df5dc/V2AAjaUAPO15qUofSpSzGh6PW4gkg2FJypyvorUwW89eU1"
+    mensagem = {
+        "title": f"Falha na Execução DAG - {context['task_instance'].dag_id}",
+        "text": f"Falha na DAG: {context['task_instance'].dag_id} na task: {context['task_instance'].task_id} VERIFICAR URGENTE!!"
+    }
+    requests.post(url, json=mensagem)
  
 ### Definindo defaults
 default_args = {
