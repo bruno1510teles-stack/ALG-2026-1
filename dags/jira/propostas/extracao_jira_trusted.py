fd1647af7ef903cf3e23507cec968fb69c7bd6a2
kevin.cardoso <kevin.cardoso@alpe.com.br>
2024-12-06 15:47:16 -0300

    ajuste df

--- dags/jira/propostas/extracao_jira_trusted.py
+++ dags/jira/propostas/extracao_jira_trusted.py
@@ -225,7 +225,7 @@ def base_details_trusted(access_params=None, **kwargs):
     # Selecionando as colunas relevantes
     jira_tratado = filtro[[
         'issue_key', 'politica_desc', 'cnpj', 'pgid', 'limite_pedido', 'limite_aprovado', 'nome_issue',
-        'tipo_proposta', 'vendedor_alpe', 'vendedor_fornecedor', 'filial_fornecedor','prioridade_desc'
+        'tipo_proposta', 'vendedor_alpe', 'vendedor_fornecedor', 'filial_fornecedor','prioridade_desc',
         'tipo_status','nome_decisor', 'status_decisao','parecer_desc', 'ramificacao_motor_desc',
         'resolvido_tratado', 'criado_tratado', 'atribuido_tratado',
         'data_criado', 'hora_criado', 
