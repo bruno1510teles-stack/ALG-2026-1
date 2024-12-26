8c727e2826e6463c839b45145680c425a6128bee
kevin.cardoso <kevin.cardoso@alpe.com.br>
2024-12-26 14:12:28 -0300

    ajuste

--- dags/jira/propostas/extracao_jira_refined.py
+++ dags/jira/propostas/extracao_jira_refined.py
@@ -193,14 +193,7 @@ def base_details_refined(access_params=None,  **kwargs):
     # Criando df para verificar a diferença de horas e minutos - Criado/ Resolvido
     df['diferença_horas_criado_resolvido'] = (df['resolvido_status_tratado'] - df['criado_tratado']).dt.total_seconds() / 3600
     df['diferença_minutos_criado_resolvido'] = (df['resolvido_status_tratado'] - df['criado_tratado']).dt.total_seconds() / 60
-
-    # Criando df para verificar a diferença de horas e minutos - Atribuido/ Resolvido
-    df['diferença_horas_atribuido_resolvido'] = (df['aguardando_execucao_tratado'] - df['resolvido_status_tratado']).dt.total_seconds() / 3600
-    df['diferença_minutos_atribuido_resolvido'] = (df['aguardando_execucao_tratado'] - df['resolvido_status_tratado']).dt.total_seconds() / 60
-    
-    
-    # Criando df para verificar a diferença de dias
-    df['diferença_dias_atribuido_resolvido'] = (df['aguardando_execucao_tratado'] - df['resolvido_status_tratado']).dt.days.astype('Int64')
+       
     
     df['diferença_horas_atribuido_resolvido'] = (df['aguardando_execucao_tratado'] - df['resolvido_status_tratado']).dt.total_seconds() / 3600
     df['diferença_minutos_atribuido_resolvido'] = (df['aguardando_execucao_tratado'] - df['resolvido_status_tratado']).dt.total_seconds() / 60
