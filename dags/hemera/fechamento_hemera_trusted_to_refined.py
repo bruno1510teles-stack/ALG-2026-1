c8204b5f9955594e64f1466475209daf4de03904
felipe.ferraz <felipe.ferraz@alpe.com.br>
2025-01-16 12:04:38 -0300

    fechamento == 05

--- dags/hemera/fechamento_hemera_trusted_to_refined.py
+++ dags/hemera/fechamento_hemera_trusted_to_refined.py
@@ -13,7 +13,7 @@ from io import BytesIO
 
 def hemera_trusted_to_refined(access_params=None,  **kwargs):
 
-    para_fechamento = '2024-06-30'
+    para_fechamento = '2024-05-31'
 
     print('Conectando com o Trino')
     ### Coletando dados da camada Trusted
