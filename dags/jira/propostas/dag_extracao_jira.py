3094c7cd8c39252ee7866d59e649f3050e0cc42c
vinicius.consultor <vincius.teixeira.consultor@alpe.com.br>
2025-01-17 10:10:15 -0500

    tqdm processo jira

--- dags/jira/propostas/dag_extracao_jira.py
+++ dags/jira/propostas/dag_extracao_jira.py
@@ -9,7 +9,6 @@ import pandas as pd
 import requests
 import pendulum
 import sys
-from tqdm import tqdm
 from datetime import datetime, timezone, timedelta
 from time import sleep
 
@@ -19,7 +18,7 @@ from extracao_jira_trusted import base_details_trusted
 from extracao_jira_refined import base_details_refined
 from propostas_boletos_vop import merge_propostas_boletos
 from jira_notif import  enviar_notif
-#from captura_propostas_jira_v2 import captura_propostas_jira
+from captura_propostas_jira_v2 import captura_propostas_jira
 
 
 
@@ -127,13 +126,11 @@ with DAG(
         provide_context=True
     )
 
-    '''
     captura_proposta_jira_v2 = PythonOperator(
         task_id='captura_proposta_jira_v2',
         python_callable=captura_propostas_jira,
         provide_context=True
     )
-    '''
 
     # BranchPythonOperator para verificar o horário e decidir qual task executar
     check_time_task = BranchPythonOperator(
@@ -154,7 +151,6 @@ with DAG(
     )
 
     # Definindo a ordem de execução das tasks
-    extracao_jira_to_raw >> extracao_jira_raw_to_trusted >> extracao_jira_to_refined >> propostas_boletos_vop_aux >> check_time_task
-    check_time_task >> [jira_notif_teams, skip_task]
-
-    #captura_proposta_jira_v2 >> 
+    captura_proposta_jira_v2
+    #extracao_jira_to_raw >> extracao_jira_raw_to_trusted >> extracao_jira_to_refined >> propostas_boletos_vop_aux >> check_time_task
+    #check_time_task >> [jira_notif_teams, skip_task]
