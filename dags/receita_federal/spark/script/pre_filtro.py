956399084bd0d2171b342d25804f7213ee8950de
felipe.ferraz <felipe.ferraz@alpe.com.br>
2025-01-20 13:47:25 -0300

    ajuste pre_filtro && carteira vendermais

--- dags/receita_federal/spark/script/pre_filtro.py
+++ dags/receita_federal/spark/script/pre_filtro.py
@@ -21,10 +21,10 @@ def cnaes_to_trusted(spark):
     hadoop_conf.set("hadoop.security.authorization", "false")
 
     # Leitura dos dados brutos da camada Trusted
-    trusted_estabelecimentos = spark.read.format("delta").load("s3a://teste-felipe/receita-federal/estabelecimentos")
-    trusted_empresas = spark.read.format("delta").load("s3a://teste-felipe/receita-federal/empresas")
-    trusted_simples = spark.read.format("delta").load("s3a://teste-felipe/receita-federal/simples")
-    trusted_natureza_juridica = spark.read.format("delta").load("s3a://teste-felipe/receita-federal/naturezas")
+    trusted_estabelecimentos = spark.read.format("delta").load("s3a://bureaus/receita-federal/estabelecimentos")
+    trusted_empresas = spark.read.format("delta").load("s3a://bureaus/receita-federal/empresas")
+    trusted_simples = spark.read.format("delta").load("s3a://bureaus/receita-federal/simples")
+    trusted_natureza_juridica = spark.read.format("delta").load("s3a://bureaus/receita-federal/naturezas")
     trusted_pep = spark.read.format("delta").load("s3a://pessoas-e-organizacoes/pep")
     trusted_socios = spark.read.format("delta").load("s3a://teste-felipe/receita-federal/socios")
     trusted_cnae = spark.read.format("delta").load("s3a://teste-felipe/receita-federal/cnaes")
