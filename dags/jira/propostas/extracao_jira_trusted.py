00b35666c80c55577a17aafcdcdee59b9b69a392
vinicius.consultor <vincius.teixeira.consultor@alpe.com.br>
2025-01-30 04:08:47 -0500

    filtro lim solicitado

--- dags/jira/propostas/extracao_jira_trusted.py
+++ dags/jira/propostas/extracao_jira_trusted.py
@@ -362,7 +362,11 @@ def jira_raw_to_trusted(access_params=None, **kwargs):
     df_resolvido['year'], df_resolvido['month'], df_resolvido['day'] = now.year, now.month, now.day
 
     # FILTRANDO APENAS APROVADOS E REPROVADOS PARA TRUSTED
-    df_resolvido = df_resolvido.loc[df_resolvido['decisao'].isin(['APROVADO', 'REPROVADO'])]
+    # FILTRANDO APENAS APROVADOS E REPROVADOS PARA TRUSTED E LIMITE SOLICITADO MENOR QUE 500.000.000
+    df_resolvido = df_resolvido.loc[
+        (df_resolvido['decisao'].isin(['APROVADO', 'REPROVADO'])) & 
+        (df_resolvido['limite_solicitado'] < 1000000000)
+    ]
 
     # SELECIONA AS COLUNAS PARA EXPORTAR
     df_final = df_resolvido[[
