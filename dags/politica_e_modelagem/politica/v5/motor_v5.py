6f9a7cfbf8ee64f633b1a5bb82b9f39094aec0f6
vinicius.consultor <vincius.teixeira.consultor@alpe.com.br>
2025-02-05 09:03:44 -0500

    fit lib json

--- dags/politica_e_modelagem/politica/v5/motor_v5.py
+++ dags/politica_e_modelagem/politica/v5/motor_v5.py
@@ -18,7 +18,7 @@ def executa_politica_v5 (access_params=None,  **kwargs):
     # Pegando DF tarefa anterior
     # Recupera o objeto ti (task instance) via kwargs
     ti = kwargs['ti']
-    base_analisar_dict = ti.xcom_pull(task_ids='pre_filtro_task_new')
+    base_analisar_dict = ti.xcom_pull(task_ids='pre_filtro')
     base_analisar = pd.DataFrame(base_analisar_dict)
 
     df_politica = base_analisar.copy()
