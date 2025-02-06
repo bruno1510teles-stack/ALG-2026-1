206556a5087a1e076acff9ef2bc017973c2f2df4
felipe.ferraz <felipe.ferraz@alpe.com.br>
2025-02-06 11:18:15 -0300

    aumentando recurso

--- dags/hemera/spark/scripts/estoque_diario_hemera.py
+++ dags/hemera/spark/scripts/estoque_diario_hemera.py
@@ -24,7 +24,7 @@ def estoque_diario_hemera(spark):
     caminho = 'estoque_csv'
 
     # Definir o caminho de cada ano
-    anos = ['2024', '2021']
+    anos = ['2024', '2023', '2022', '2021']
 
     # Lista para armazenar os DataFrames de cada ano
     df_list = []
@@ -194,9 +194,9 @@ if __name__ == "__main__":
         .config("spark.sql.encoding", "latin1") \
         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
         .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
-        .config("spark.driver.memory", "2g") \
-        .config("spark.executor.memory", "4g") \
-        .config("spark.executor.cores", "1") \
+        .config("spark.driver.memory", "3g") \
+        .config("spark.executor.memory", "5g") \
+        .config("spark.executor.cores", "2") \
         .config("spark.sql.shuffle.partitions", "100") \
     .getOrCreate()
 
