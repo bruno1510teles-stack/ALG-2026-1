a27c74721caea423e3234b61f63dede60aab54dd
felipe.ferraz <felipe.ferraz@alpe.com.br>
2025-02-11 12:10:34 -0300

    ajustando nome dag politicas

--- dags/politica_e_modelagem/politica/v4/zerar_limite/dag_zerar_limites_v4.py
+++ dags/politica_e_modelagem/politica/v4/zerar_limite/dag_zerar_limites_v4.py
@@ -48,7 +48,7 @@ default_args = {
 
 # Definindo a DAG
 with DAG(
-    dag_id='politica_v4',
+    dag_id='politica_v_4',
     start_date=days_ago(1),
     schedule_interval=None,
     default_args=default_args,
