2e5ea6c1d81c2e878f1a5e8f7f748a3cd6e27ebb
gustavo.paulino <gustavo.gustavo.paulino@alpe.com.br>
2025-02-13 14:13:21 -0300

    Atualizando dados cadastrais

--- dags/receita_federal/spark/script/dados_cadastrais.py
+++ dags/receita_federal/spark/script/dados_cadastrais.py
@@ -6,7 +6,7 @@ import os
 from datetime import datetime, timezone, timedelta
 
 
-def cnaes_to_trusted(spark):
+def dados_cadastrais_to_refined(spark):
 
     hadoop_conf = spark.sparkContext._jsc.hadoopConfiguration()
     hadoop_conf.set("fs.s3a.access.key", os.getenv('MINIO_TRUSTED_ACCESS_KEY'))
@@ -21,8 +21,11 @@ def cnaes_to_trusted(spark):
     hadoop_conf.set("hadoop.security.authorization", "false")
 
     # Leitura dos dados brutos da camada Trusted
-    estabelecimentos = spark.read.format("delta").load("s3a://teste-felipe/receita-federal/estabelecimentos")
-    empresas = spark.read.format("delta").load("s3a://teste-felipe/receita-federal/empresas")
+    # Leitura dos dados brutos da camada Trusted e tratando colunas
+    estabelecimentos = spark.read.format("delta").load("s3a://teste-felipe/receita-federal/estabelecimentos")\
+            .withColumn("data_situacao_cadastral", col("data_situacao_cadastral").cast("date"))
+    empresas = spark.read.format("delta").load("s3a://teste-felipe/receita-federal/empresas")\
+            .withColumn("capital_social_empresa", col("capital_social_empresa").cast("double"))
     simples = spark.read.format("delta").load("s3a://teste-felipe/receita-federal/simples")
     natureza = spark.read.format("delta").load("s3a://teste-felipe/receita-federal/naturezas")
     cnae = spark.read.format("delta").load("s3a://teste-felipe/receita-federal/cnaes")
@@ -30,7 +33,7 @@ def cnaes_to_trusted(spark):
     print("Arquivos lidos")
     
 
-    # Criação da tabela final
+     #Criação da tabela final
     dados_cadastrais = (
         estabelecimentos.alias("e")
         .join(empresas.alias("emp"), col("e.cnpj_raiz") == col("emp.cnpj_raiz"), "left")
@@ -42,12 +45,12 @@ def cnaes_to_trusted(spark):
             col("e.cnpj_raiz"),
             col("e.documento_sem_formatacao").alias("cnpj_sem_formatacao"),
             col("e.documento_formatado").alias("cnpj_formatado"),
-            when(col("e.is_matriz") == True, "Sim").otherwise("Nao").alias("matriz"),
+            when(col("e.is_matriz") == True, "Sim").otherwise("Nao").alias("flag_matriz"),
             col("emp.razao_social"),
             col("e.nome_fantasia"),
             col("e.situacao_cadastral"),
-            to_date(col("e.data_situacao_cadastral"),"yyyy-MM-dd").alias("data_situacao_cadastral"),
-            to_date(col("e.data_inicio_atividade"),"yyyy-MM-dd").alias("data_fundacao"),
+            to_date(col("e.data_situacao_cadastral").cast("date"), "yyyy-MM-dd").alias("data_situacao_cadastral"),
+            to_date(col("e.data_inicio_atividade"), "yyyy-MM-dd").alias("data_fundacao"),
             col("e.cnae_principal").alias("cnae_principal_codigo"),
             col("cnae.descricao").alias("cnae_principal_descricao"),
             col("e.cnae_secundaria"),
@@ -59,26 +62,24 @@ def cnaes_to_trusted(spark):
             col("e.uf"), 
             col("m.descricao").alias("municipio"),
             col("e.situacao_especial"), 
-            to_date(col("e.data_sitaucao_especial"),"yyyy-MM-dd").alias("data_sitaucao_especial"),
+            to_date(col("e.data_sitaucao_especial"), "yyyy-MM-dd").alias("data_situacao_especial"),
             col("emp.natureza_juridica").alias("natureza_juridica_codigo"),
             col("nat.descricao").alias("descricao_natureza_juridica"),
-            col("emp.capital_social_empresa").cast("double").alias("capital_social_empresa"), 
+            col("emp.capital_social_empresa").cast("double").alias("capital_social_empresa"),
             col("emp.porte_empresa"),
             when(col("s.is_simples") == True, "Sim").otherwise("Nao").alias("flag_simples"),
-            to_date(col("s.data_opcao_pelo_simples"),"yyyy-MM-dd").alias("data_opcao_pelo_simples"),
-            col("s.data_exclusao_simples"),
+            to_date(col("s.data_opcao_pelo_simples"), "yyyy-MM-dd").alias("data_opcao_simples"),
+            to_date(col("s.data_exclusao_simples"), "yyyy-MM-dd").alias("data_exclusao_simples"),
             when(col("s.is_mei") == True, "Sim").otherwise("Nao").alias("flag_mei"),
-            to_date(col("s.data_opcao_pelo_mei"),"yyyy-MM-dd").alias("data_opcao_pelo_mei"),
-            col("s.data_exclusao_mei"),
+            to_date(col("s.data_opcao_pelo_mei"), "yyyy-MM-dd").alias("data_opcao_mei"),
+            to_date(col("s.data_exclusao_mei"), "yyyy-MM-dd").alias("data_exclusao_mei"),
             col("e.data_ref"),
-            date_format(current_timestamp(), "yyyy-MM-dd").alias("Atualizado em")
+            date_format(current_timestamp(), "yyyy-MM-dd").alias("Atualizado_em")
+            
 
         )
     )
     
-    print("Total de registros:", dados_cadastrais.count())
-    dados_cadastrais.show(10)
-
 
     # Salvando arquivos
     hadoop_conf.set("fs.s3a.access.key", os.getenv('MINIO_REFINED_ACCESS_KEY'))
@@ -107,12 +108,13 @@ if __name__ == "__main__":
         .config("spark.sql.encoding", "latin1") \
         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
         .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+        .config("spark.sql.parquet.datetimeRebaseModeInWrite","LEGACY") \
         .config("spark.driver.memory", "4g") \
         .config("spark.executor.memory", "8g") \
-        .config("spark.executor.cores", "1") \
+        .config("spark.executor.cores", "2") \
         .config("spark.sql.shuffle.partitions", "100") \
     .getOrCreate()
 
     spark.sparkContext.setLogLevel("ERROR")
 
-    cnaes_to_trusted(spark)
+    dados_cadastrais_to_refined(spark)
