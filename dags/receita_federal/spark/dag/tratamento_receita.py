3e61f9add85675d22b7fbb19674fd74d6d5143bd
gustavo.paulino <gustavo.gustavo.paulino@alpe.com.br>
2025-02-13 14:17:16 -0300

    Inserindo task dados cadastrais

--- dags/receita_federal/spark/dag/tratamento_receita.py
+++ dags/receita_federal/spark/dag/tratamento_receita.py
@@ -128,14 +128,6 @@ with DAG(
         execution_timeout=timedelta(minutes=120)
     )
 
-    dados_cadastrais = SparkKubernetesOperator(
-        task_id='dados-cadastrais',
-        application_file='dados-cadastrais-spark-app.yaml',
-        namespace='spark',
-        kubernetes_conn_id='kubernetes_default',
-        do_xcom_push=True,
-    )
-
     pre_filtro = SparkKubernetesOperator(
         task_id='pre_filtro',
         application_file='pre-filtro-spark-app.yaml',
@@ -145,4 +137,12 @@ with DAG(
         execution_timeout=timedelta(minutes=120)
     )
 
-    cnae >> empresas >> estabelecimentos >> motivos >> municipios >> naturezas >> paises >> qualificacoes >> simples >> socios >> dados_cadastrais >> pre_filtro
+    dados_cadastrais = SparkKubernetesOperator(
+        task_id='dados-cadastrais',
+        application_file='dados-cadastrais-spark-app.yaml',
+        namespace='spark',
+        kubernetes_conn_id='kubernetes_default',
+        do_xcom_push=True,
+    )
+
+    cnae >> empresas >> estabelecimentos >> motivos >> municipios >> naturezas >> paises >> qualificacoes >> simples >> socios >> pre_filtro >> dados_cadastrais
