465565e4bf524cdd8e182431c84c95854fe36f2f
felipe.ferraz <felipe.ferraz@alpe.com.br>
2025-02-17 10:10:58 -0300

    "jira_max_result": 15 > 50

--- dags/politica_e_modelagem/hub_politicas/dags/dag_hub_politicas.py
+++ dags/politica_e_modelagem/hub_politicas/dags/dag_hub_politicas.py
@@ -36,7 +36,7 @@ access_params = {
     "jira_api_token": Variable.get('JIRA_API_TOKEN'),
     "jira_api_user": Variable.get('JIRA_API_USER'),
     "jira_project": Variable.get('JIRA_PROJECT'),
-    "jira_max_result": 15
+    "jira_max_result": 50
     }
 
 def notificar_falha_teams(context):
