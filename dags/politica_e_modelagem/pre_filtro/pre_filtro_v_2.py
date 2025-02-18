4af2bd929c96b06591967601695b2a5684e35922
felipe.ferraz <felipe.ferraz@alpe.com.br>
2025-02-18 10:11:27 -0300

    voltando regra pre-filtro - normal

--- dags/politica_e_modelagem/pre_filtro/pre_filtro_v_2.py
+++ dags/politica_e_modelagem/pre_filtro/pre_filtro_v_2.py
@@ -377,10 +377,10 @@ def analise_pre_filtro(access_params=None,  **kwargs):
 
     # Criando Resposta
     response_map = {
-        'REPROVADO': ['PF CNPJ IRREGULAR', 'PF REPROVA < 60 DIAS', 'PF RJ', 'PF PEP', 'PF MEI', 'PF CNAE', 'PF NATUREZA JURIDICA', 'PF FUNDACAO < 2 ANOS'],
+        'REPROVADO': ['PF CNPJ IRREGULAR', 'PF REPROVA < 60 DIAS', 'PF RJ', 'PF PEP', 'PF MEI', 'PF CNAE', 'PF NATUREZA JURIDICA', 'PF FUNDACAO < 2 ANOS', 'PF SOCIO < 2 ANOS'],
         'mantido' : ['PF LIMITE SOLICITADO <= ATUAL', 'PF - UTILIZAÇÃO DE LIMITE MÍNIMA NÃO ATINGIDA'],
         'MESA': ['PF MESA', 'PF CONSORCIO/CONSTRUTORA/SPE/SA', 'PF SOCIO PJ', 'PF BLOQUEIO ALPE'],
-        'SEGUE': ['PF SEGUE', 'PF SOCIO < 2 ANOS']
+        'SEGUE': ['PF SEGUE']
     }
     # Aplicar as respostas
     for response, values in response_map.items():
