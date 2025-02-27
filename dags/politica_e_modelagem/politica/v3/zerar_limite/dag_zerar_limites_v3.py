5306722ccb95f55cf833f6469f30eb0667efe6f7
Vinicius Moraes <vinicius.teixeira@alpe.com.br>
2025-02-27 11:04:06 -0300

    ajuste nome task - v3 e v4

--- dags/politica_e_modelagem/politica/v3/zerar_limite/dag_zerar_limites_v3.py
+++ dags/politica_e_modelagem/politica/v3/zerar_limite/dag_zerar_limites_v3.py
@@ -115,7 +115,7 @@ with DAG(
 
     # Execução da política
     executa_politica = PythonOperator(
-        task_id = "executa_politica",
+        task_id = "politica_task",
         python_callable = executa_politica.execucao_politica,
         op_kwargs = {'access_params': access_params},
         provide_context = True
--- dags/politica_e_modelagem/politica/v4/zerar_limite/dag_zerar_limites_v4.py
+++ dags/politica_e_modelagem/politica/v4/zerar_limite/dag_zerar_limites_v4.py
@@ -110,7 +110,7 @@ with DAG(
 
     # Execução da política
     executa_politica = PythonOperator(
-        task_id = "executa_politica",
+        task_id = "politica_task",
         python_callable = executa_politica.execucao_politica,
         op_kwargs = {'access_params': access_params},
         provide_context = True
