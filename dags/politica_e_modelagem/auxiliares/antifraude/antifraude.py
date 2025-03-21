019a6ba33bbbe1f9ee0f0400aa253623d425fcd0
Vinicius Moraes <vinicius.teixeira@alpe.com.br>
2025-03-21 16:57:22 -0300

    ajuste para passar pelo filtro de endereço igual

--- dags/politica_e_modelagem/auxiliares/antifraude/antifraude.py
+++ dags/politica_e_modelagem/auxiliares/antifraude/antifraude.py
@@ -65,7 +65,8 @@ def anti_fraude(access_params=None,  **kwargs):
         (df_antifraude['flag_mudanca_endereco'] == 'Sim', 'AF - MUDANÇA ENDEREÇO'),
         (df_antifraude['flag_mudanca_cidade'] == 'Sim', 'AF - MUDANÇA CIDADE'),
         (df_antifraude['flag_mudanca_estado'] == 'Sim', 'AF - MUDANÇA ESTADO'),
-        (df_antifraude['flag_endereco_igual'] == 'Sim', 'AF - ENDEREÇO IGUAL')
+        #(df_antifraude['flag_endereco_igual'] == 'Sim', 'AF - ENDEREÇO IGUAL') descomentar no futuro
+        (df_antifraude['flag_endereco_igual'] == 'Sim', 'AF SEGUE')
     ]
 
     for condition, value in condicoes:
