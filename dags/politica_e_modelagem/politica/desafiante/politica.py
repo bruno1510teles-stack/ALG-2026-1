4ce98af870b07cca69512392e0c413ba32e6e833
Vinicius Moraes <vinicius.teixeira@alpe.com.br>
2025-03-24 17:52:39 -0300

    fit parecer

--- dags/politica_e_modelagem/politica/desafiante/politica.py
+++ dags/politica_e_modelagem/politica/desafiante/politica.py
@@ -767,7 +767,7 @@ def executa_politica (access_params=None,  **kwargs):
             (df_resultado['ramificacao_final'] == 'A8 | A3')
         ),
         "Motor - Cliente pré aprovado por crédito, aguardando validação de fraude",
-        df_resultado['parecer']
+        np.nan
     )
 
 
