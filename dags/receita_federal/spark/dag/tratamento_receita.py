374baa85ab3cd06f577710fd46da63050236d829
gustavo.paulino <gustavo.gustavo.paulino@alpe.com.br>
2025-04-02 15:58:26 -0300

    Ajuste dag

--- dags/receita_federal/spark/dag/tratamento_receita.py
+++ dags/receita_federal/spark/dag/tratamento_receita.py
@@ -144,7 +144,7 @@ with DAG(
         kubernetes_conn_id='kubernetes_default',
         do_xcom_push=True,
         retries = 3, # Ajustando retries
-        retry_delay=timedelta(minutes=5) # Ajuste
+        retry_delay=timedelta(minutes=120) # Ajuste
     )
 
     cnae >> empresas >> estabelecimentos >> motivos >> municipios >> naturezas >> paises >> qualificacoes >> simples >> socios >> pre_filtro >> dados_cadastrais
