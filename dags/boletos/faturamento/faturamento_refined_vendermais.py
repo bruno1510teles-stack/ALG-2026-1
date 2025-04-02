348a0746b8a3a3bb735f888c0a6b33be9283aafe
felipe.ferraz <felipe.ferraz@alpe.com.br>
2025-04-02 12:15:38 -0300

    OR status_fatura_sefaz IS NULL

--- dags/boletos/faturamento/faturamento_refined_vendermais.py
+++ dags/boletos/faturamento/faturamento_refined_vendermais.py
@@ -44,7 +44,7 @@ def faturamento_to_refined(access_params=None,  **kwargs):
         data_fatura, 
         CONCAT(cnpj_sacado, cnpj_cedente, numero_nfe) as chave,
         SUM(valor_fatura) AS valor_fatura, 
-        SUM(CASE WHEN status_fatura_sefaz <> 'CANCELED' THEN valor_fatura ELSE 0 END) AS valor_fatura_pos_sefaz
+        SUM(CASE WHEN status_fatura_sefaz <> 'CANCELED' OR status_fatura_sefaz IS NULL THEN valor_fatura ELSE 0 END) AS valor_fatura_pos_sefaz
     FROM 
         deltalaketrusted.payments.faturamento ft
     GROUP BY
