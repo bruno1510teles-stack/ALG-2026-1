582a7bd8b994811b58e5abadeefe3f4b630f5465
Vinicius Moraes <vinicius.teixeira@alpe.com.br>
2025-04-14 13:22:50 -0300

    fit markdown

--- dags/politica_e_modelagem/reporte_motor/report_desafiante_hora.py
+++ dags/politica_e_modelagem/reporte_motor/report_desafiante_hora.py
@@ -130,13 +130,13 @@ def report_motor_desafiante_hora_hora (access_params=None):
     # Criar o markdown com o DataFrame ordenado e os percentuais
     markdown = (
         "📊 Resumo Diário de Propostas - Política Desafiante - TESTE\n\n"
-        f"{invisible_space}\n"
+        ".\n"
         f"📅 Data Referência: {data_execucao}\n\n"
-        f"{invisible_space}\n"
-        f"🧾 (%) MESA: {percent_mesa:.2f}%\n"
-        f"✅ (%) APROVADAS MOTOR: {percent_aprov_motor:.2f}%\n"
-        f"❌ (%) REPROVADAS MOTOR: {percent_reprov_motor:.2f}%\n"
-        f"{invisible_space}\n"
+        ".\n"
+        f"🧾 (%) MESA: {percent_mesa:.2f}%\n\n"
+        f"✅ (%) APROVADAS MOTOR: {percent_aprov_motor:.2f}%\n\n"
+        f"❌ (%) REPROVADAS MOTOR: {percent_reprov_motor:.2f}%\n\n"
+        ".\n"
         "```\n"
         + tabela_formatada +
         "\n```"
