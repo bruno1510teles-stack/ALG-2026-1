01e409969c9b2ae85478cb05fd3ba70e53a6d1a5
Vinicius Moraes <vinicius.teixeira@alpe.com.br>
2025-04-14 10:42:35 -0300

    comentando reporte motor desafiante por propostas teams

--- dags/politica_e_modelagem/politica/desafiante/dag_politica_desafiante.py
+++ dags/politica_e_modelagem/politica/desafiante/dag_politica_desafiante.py
@@ -183,14 +183,17 @@ with DAG(
         python_callable=lambda: sleep(10),  # Espera por 10 segundos
     )
 
+    '''
     # Enviando dados para o Kafka
     enviar_alerta_teams = PythonOperator(
         task_id="envia_msg_teams",
         python_callable=envia_alerta_teams.envia_alerta_teams,
         op_kwargs={'access_params': access_params},
         provide_context=True,
-    )
+    )'
+    '''
 
     # Definindo a ordem de execução das tasks
-    captura_proposta >> anti_fraude >> pre_filtro >> serasa >> aguarde >> anti_fraude_serasa >> politica_desafiante >> enviar_kafka >> enviar_alerta_teams
+    captura_proposta >> anti_fraude >> pre_filtro >> serasa >> aguarde >> anti_fraude_serasa >> politica_desafiante >> enviar_kafka 
+    #>> enviar_alerta_teams
 
