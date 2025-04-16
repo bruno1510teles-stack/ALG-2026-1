ed16a0d2b7dae6bd99f016b40d2f79e86fc231ee
felipe.ferraz <felipe.ferraz@alpe.com.br>
2025-04-16 09:43:16 -0300

    socio_pj

--- dags/politica_e_modelagem/pre_filtro/pre_filtro.py
+++ dags/politica_e_modelagem/pre_filtro/pre_filtro.py
@@ -413,7 +413,7 @@ def analise_pre_filtro(access_params=None,  **kwargs):
         response_map = {
             'REPROVADO': ['PF CNPJ IRREGULAR', 'PF REPROVA < 60 DIAS', 'PF RJ', 'PF PEP', 'PF MEI', 'PF CNAE', 'PF NATUREZA JURIDICA', 'PF FUNDACAO < 2 ANOS'],
             'mantido' : ['PF LIMITE SOLICITADO <= ATUAL', 'PF - UTILIZAÇÃO DE LIMITE MÍNIMA NÃO ATINGIDA'],
-            'MESA': ['PF MESA', 'PF CONSORCIO/CONSTRUTORA/SPE/SA', 'PF SOCIO < 2 ANOS', 'PF BLOQUEIO ALPE'],
+            'MESA': ['PF MESA', 'PF CONSORCIO/CONSTRUTORA/SPE/SA', 'PF SOCIO < 2 ANOS', 'PF BLOQUEIO ALPE', 'PF SOCIO PJ'],
             'SEGUE': ['PF SEGUE']
         }
         # Aplicar as respostas
