feb317d42d34a45ba5771cd26aa28028a1b2b96e
felipe.ferraz <felipe.ferraz@alpe.com.br>
2025-05-06 15:00:37 -0300

    print para debug

--- dags/politica_e_modelagem/auxiliares/antifraude/antifraude.py
+++ dags/politica_e_modelagem/auxiliares/antifraude/antifraude.py
@@ -114,6 +114,9 @@ def anti_fraude(access_params=None,  **kwargs):
     
     df = df_antifraude.merge(base_analisar[['cnpj_sem_formatacao', 'issue_jira', 'inad_alpe', 'pgid', 'limite_solicitado']], on = ['cnpj_sem_formatacao'], how = 'left')
 
+    print("df pós cruzamento")
+    print(df)
+
     df['limite_solicitado'] = df['limite_solicitado'].astype(str)
 
     # Tratando limite_solicitado para task seguinte
