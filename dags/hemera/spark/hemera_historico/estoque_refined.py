b35e99d1afdcd0ea12d03b4a5512705d3053c001
Vinicius Moraes <vinicius.teixeira@alpe.com.br>
2025-05-11 23:45:38 -0300

    fit

--- dags/hemera/spark/hemera_historico/estoque_refined.py
+++ dags/hemera/spark/hemera_historico/estoque_refined.py
@@ -213,8 +213,8 @@ if __name__ == "__main__":
         .config("spark.sql.encoding", "latin1") \
         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
         .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
-        .config("spark.driver.memory", "5g") \
-        .config("spark.executor.memory", "8g") \
+        .config("spark.driver.memory", "3g") \
+        .config("spark.executor.memory", "5g") \
         .config("spark.executor.cores", "4") \
         .config("spark.sql.shuffle.partitions", "100") \
     .getOrCreate()
