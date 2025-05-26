f5109c3dc2de59b1816596b6ff537c0224489686
Vinicius Moraes <vinicius.teixeira@alpe.com.br>
2025-05-26 14:19:08 -0300

    getenv raw - estoque hemera

--- dags/hemera/spark/hemera_rotina/estoque_diario_trusted.py
+++ dags/hemera/spark/hemera_rotina/estoque_diario_trusted.py
@@ -39,10 +39,11 @@ def estoque_diario_trusted (access_params=None, **kwargs):
 
 
     minio_raw = Minio(
-        access_params['endpoint_url_raw'],
-        access_key=access_params['aws_access_key_id_raw'],
-        secret_key=access_params['aws_secret_access_key_raw'],
+        os.getenv('MINIO_RAW_ENDPOINT'),
+        access_key = os.getenv('MINIO_RAW_ACCESS_KEY'),
+        secret_key = os.getenv('MINIO_RAW_SECRET_KEY'),
     )
+
 
     # Connection validation
     try:
