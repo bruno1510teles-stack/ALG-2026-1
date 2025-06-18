5909fdf5540e8ce0c92954c110bcc5777d6eb9eb
felipe.ferraz <felipe.ferraz@alpe.com.br>
2025-06-18 14:42:50 -0300

    ajustando retries receita

--- dags/receita_federal/spark/dag/tratamento_receita.py
+++ dags/receita_federal/spark/dag/tratamento_receita.py
@@ -27,7 +27,7 @@ def notificar_falha_teams(context):
 default_args = {
     'owner': 'Felipe Ferraz',
     'start_date': days_ago(1),
-    "retries": 3,
+    "retries": 1,
     "retry_delay": timedelta(minutes=5)#,
     #"on_failure_callback": notificar_falha_teams
 } 
@@ -134,7 +134,7 @@ with DAG(
         namespace='spark',
         kubernetes_conn_id='kubernetes_default',
         do_xcom_push=True,
-        retries = 3, # Ajustando retries
+        retries = 1, # Ajustando retries
         execution_timeout=timedelta(minutes=120)
     )
 
@@ -144,7 +144,7 @@ with DAG(
         namespace='spark',
         kubernetes_conn_id='kubernetes_default',
         do_xcom_push=True,
-        retries = 3, # Ajustando retries
+        retries = 1, # Ajustando retries
         retry_delay=timedelta(minutes=120) # Ajuste
     )
 
