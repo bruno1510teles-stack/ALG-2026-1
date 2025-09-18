d160f731a2a8bb25591f3a1c09f3c89f85983a4c
Vinicius Moraes <vinicius.teixeira@alpe.com.br>
2025-09-18 17:04:15 -0300

    retirando timezone

--- dags/boletos/vendermais/acumulada_boletos_vop.py
+++ dags/boletos/vendermais/acumulada_boletos_vop.py
@@ -84,6 +84,7 @@ def boletos_raw_to_trusted_acumulada (access_params=None,  **kwargs):
 
     df_acumulado_boletos['data_ref'] = df_acumulado_boletos['data_ref'].dt.to_timestamp()
 
+    df_acumulado_boletos['data_ref'] = (df_acumulado_boletos['data_ref'].dt.to_timestamp(how="start") .dt.tz_localize(None))
 
     ### RESET INDEX
     df_acumulado_boletos = df_acumulado_boletos.reset_index(drop=True)
