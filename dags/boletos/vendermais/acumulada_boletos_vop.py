7394bcb40fd803171a861840d5f17e7e0fc1a345
Vinicius Moraes <vinicius.teixeira@alpe.com.br>
2025-09-19 09:49:53 -0300

    data-ref referenciando ultimo dia do mes

--- dags/boletos/vendermais/acumulada_boletos_vop.py
+++ dags/boletos/vendermais/acumulada_boletos_vop.py
@@ -82,7 +82,12 @@ def boletos_raw_to_trusted_acumulada (access_params=None,  **kwargs):
     colunas = ["data_ref"] + [c for c in df_acumulado_boletos.columns if c != "data_ref"]
     df_acumulado_boletos = df_acumulado_boletos[colunas]
 
-    df_acumulado_boletos['data_ref'] = df_acumulado_boletos['data_ref'].dt.to_timestamp()
+
+    df_acumulado_boletos['data_ref'] = (
+        df_acumulado_boletos['data_ref']
+        .dt.to_timestamp(how="end")
+        .dt.date
+    )
 
     ### RESET INDEX
     df_acumulado_boletos = df_acumulado_boletos.reset_index(drop=True)
